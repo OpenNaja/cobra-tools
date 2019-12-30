f078c41a427d19931ac6aa5b69dc9a64f9eba786
harlequinzeg0 <j.wardftw@gmail.com>
2019-12-30 04:10:03 -0600

    Update hasher.py

--- modules/hasher.py
+++ modules/hasher.py
@@ -25,27 +25,20 @@ def djbb(s):
 def dat_hasher(archive):
 	print("\nHashing from archive",archive.archive_index)
 	for sized_str_entry in archive.header_entries:
-		new_name = sized_str_entry.name
-		new_name[0] = "z"
+		new_name = "z"+sized_str_entry.name[1:]
 		new_hash = djbb(new_name)
 		print(sized_str_entry.name,sized_str_entry.file_hash,new_name,new_hash)
 		sized_str_entry.file_hash = new_hash
 	for data_entry in archive.data_entries:
-		new_name = data_entry.name
-		new_name[0] = "z"
+		new_name = "z"+data_entry.name[1:]
 		new_hash = djbb(new_name)
 		data_entry.file_hash = new_hash
 	for set_entry in archive.set_header.sets:
-		new_name = set_entry.name
-		new_name[0] = "z"
+		new_name = "z"+set_entry.name[1:]
 		new_hash = djbb(new_name)
 		set_entry.file_hash = new_hash
 	for asset_entry in archive.set_header.assets:
-		new_name = asset_entry.name
-		new_name[0] = "z"
+		new_name = "z"+asset_entry.name[1:]
 		new_hash = djbb(new_name)
 		asset_entry.file_hash = new_hash
 	print("/nDone!")
-	
-	
-
