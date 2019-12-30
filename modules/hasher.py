ceead287afcdb64e60c4217718d1abe98e5119ef
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-12-30 21:59:06 +0100

    Refactor hashing again

--- modules/hasher.py
+++ modules/hasher.py
@@ -10,33 +10,21 @@ def djbb(s):
 
 def dat_hasher(archive, name_tups, header_files, header_textures):
 	print("\nHashing from archive",archive.archive_index)
-	for entry_list in (archive.header_entries, archive.sized_str_entries, archive.data_entries, archive.set_header.sets, archive.set_header.assets):
+	for entry_list in (archive.header_entries,
+					   archive.sized_str_entries,
+					   archive.data_entries,
+					   archive.set_header.sets,
+					   archive.set_header.assets,
+					   header_files,
+					   header_textures):
 		for entry in entry_list:
-			# new_name = entry.basename
+			if "bad hash" in entry.name:
+				print("Skipping",entry.name, entry.file_hash)
+				continue
 			new_name, ext = os.path.splitext(entry.name)
 			for old, new in name_tups:
 				new_name = new_name.replace(old, new)
 			new_hash = djbb(new_name)
 			print(entry.name, entry.file_hash, new_name, new_hash)
 			entry.file_hash = new_hash
-	print("\nFiles")
-	for file_entry in header_files:  
-		new_name = file_entry.name
-		for old, new in name_tups:
-			new_name = new_name.replace(old, new)
-		new_hash = djbb(new_name)
-		print(file_entry.name,file_entry.hash,new_name,new_hash)
-		file_entry.hash = new_hash
-	print("\nTextures")
-	for texture_entry in header_textures: 
-		if "bad hash" != texture_entry.name:
-			new_name = texture_entry.name
-			for old, new in name_tups:
-				new_name = new_name.replace(old, new)
-			new_hash = djbb(new_name)
-			texture_entry.hash = new_hash
-		else:
-			new_hash = texture_entry.hash
-			new_name = texture_entry.name
-		print(texture_entry.name, texture_entry.hash, new_name,new_hash)
 	print("Done!")
