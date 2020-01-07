e8da6844b2e37814f3058956ef8a4796e6219e4e
harlequinzeg0 <j.wardftw@gmail.com>
2020-01-07 10:57:03 -0600

    Fix to hash code
    header ad sized str use basename, file entry and tex entry use name, don't need to do splitting.

--- modules/hasher.py
+++ modules/hasher.py
@@ -12,13 +12,9 @@ def djbb(s):
 
 def dat_hasher(archive, name_tups, header_files, header_textures):
 	print("\nHashing from archive", archive.archive_index)
-	for entry_list in (archive.header_entries,
-					   archive.sized_str_entries,
-					   archive.data_entries,
+	for entry_list in (archive.data_entries,
 					   archive.set_header.sets,
-					   archive.set_header.assets,
-					   header_files,
-					   header_textures):
+					   archive.set_header.assets):
 		for entry in entry_list:
 			if "bad hash" in entry.name:
 				print("Skipping",entry.name, entry.file_hash)
@@ -27,6 +23,35 @@ def dat_hasher(archive, name_tups, header_files, header_textures):
 			for old, new in name_tups:
 				new_name = new_name.replace(old, new)
 			new_hash = djbb(new_name)
-			print(entry.name, entry.file_hash, new_name, new_hash)
+			print(entry.name, entry.file_hash, hex(entry.file_hash), new_name, new_hash, hex(new_hash))
 			entry.file_hash = new_hash
+            
+            
+            
+	for entry_list in (archive.header_entries,
+						archive.sized_str_entries):
+		for entry in entry_list:
+			if "bad hash" in entry.name:
+				print("Skipping",entry.name, entry.file_hash)
+				continue
+			new_name = entry.basename
+			for old, new in name_tups:
+				new_name = new_name.replace(old, new)
+			new_hash = djbb(new_name)
+			print(entry.name, entry.file_hash, hex(entry.file_hash), new_name, new_hash, hex(new_hash))
+			entry.file_hash = new_hash
+
+	for entry_list in (header_files,
+						header_textures):
+		for entry in entry_list:
+			if "bad hash" in entry.name:
+				print("Skipping",entry.name, entry.file_hash)
+				continue
+			new_name = entry.name
+			for old, new in name_tups:
+				new_name = new_name.replace(old, new)
+			new_hash = djbb(new_name)
+			print(entry.name, entry.file_hash, hex(entry.file_hash), new_name, new_hash, hex(new_hash))
+			entry.file_hash = new_hash					                
+                       
 	print("Done!")
