42ed82e310885c39dfaa7b70973028d3fd9beb2c
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-09-20 19:47:55 +0200

    Add revorb so VLC plays oggs

--- util/texconv.py
+++ util/texconv.py
@@ -4,6 +4,7 @@ util_dir = os.path.dirname(__file__)
 BINARY = os.path.normpath( os.path.join( util_dir , "texconv/texconv.exe") )
 ww2ogg = os.path.normpath( os.path.join( util_dir, "ww2ogg/ww2ogg.exe") )
 pcb = os.path.normpath( os.path.join( util_dir, "ww2ogg/packed_codebooks_aoTuV_603.bin") )
+revorb = os.path.normpath( os.path.join( util_dir, "revorb/revorb.exe") )
 # print(BINARY)
 # print(os.path.exists(BINARY))
 
@@ -17,12 +18,11 @@ def wem_to_ogg( wem_files, out_dir, show_dds):
 	for wem_file in wem_files:
 		print("wem 2 ogg", wem_file, out_dir, show_dds)
 		ogg_name = os.path.splitext(os.path.basename(wem_file))[0]+".ogg"
-
-		run_smart([ww2ogg, wem_file, "-o", os.path.join(out_dir, ogg_name), "--pcb", pcb, ])
+		ogg_file = os.path.join(out_dir, ogg_name)
+		run_smart([ww2ogg, wem_file, "-o", ogg_file, "--pcb", pcb, ])
+		run_smart([revorb, ogg_file])
 	clear_tmp( wem_file, show_dds)
-	# in_dir, in_name = os.path.split(wem_file)
-	# name = os.path.splitext(in_name)[0]
-	# return os.path.join(out_dir, name + '.ogg')
+
 
 def dds_to_png( dds_file_path, out_dir, height, show_dds):
 	"""Converts a DDS file given by a path to a PNG file"""
