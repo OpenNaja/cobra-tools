c9eb318b95181011f796d84af897f3c21613afa0
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-09-27 16:36:11 +0200

    Add other type for bnk

---
+++ generated/formats/bnk/compound/TypeOther.py
@@ -0,0 +1,31 @@
+import typing
+
+
+class TypeOther:
+
+# generic
+
+	# length of this section
+	length: int
+
+	# id of this Sound SFX object
+	raw: typing.List[int]
+
+	def __init__(self, arg=None, template=None):
+		self.arg = arg
+		self.template = template
+
+	def read(self, stream):
+		self.length = stream.read_uint()
+		self.raw = [stream.read_byte() for _ in range(self.length)]
+
+	def write(self, stream):
+		stream.write_uint(self.length)
+		for item in self.raw: stream.write_byte(item)
+
+	def __repr__(self):
+		s = 'TypeOther'
+		s += '\n	* length = ' + self.length.__repr__()
+		s += '\n	* raw = ' + self.raw.__repr__()
+		s += '\n'
+		return s
