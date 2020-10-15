e82248a605fb3a14fcbd4e83e9e2b57b12b209c0
harlequinzeg0 <j.wardftw@gmail.com>
2020-10-15 03:35:43 -0500

    quality of life change, select what file types extract

--- modules/extract.py
+++ modules/extract.py
@@ -11,7 +11,7 @@ from modules.util import write_sized_str
 from util import texconv
 
 
-def extract(archive, show_dds, only_types=[], progress_callback=None):
+def extract(archive, extract_fdb, extract_lua, extract_anim, extract_model, extract_tex, extract_shader, extract_text, extract_aux, extract_fct, extract_misc, show_dds, only_types=[], progress_callback=None):
 	"""Extract the files, after all archives have been read"""
 	# the actual export, per file type
 	error_files = []
@@ -28,43 +28,43 @@ def extract(archive, show_dds, only_types=[], progress_callback=None):
 			# ignore types in the count that we export from inside other type exporters
 			if sized_str_entry.ext in exported_types:
 				continue
-			elif sized_str_entry.ext == "banis":
+			elif sized_str_entry.ext == "banis" and extract_anim == True:
 				write_banis(archive, sized_str_entry)
-			elif sized_str_entry.ext == "bani":
+			elif sized_str_entry.ext == "bani" and extract_anim == True:
 				write_bani(archive, sized_str_entry)
-			elif sized_str_entry.ext == "manis":
+			elif sized_str_entry.ext == "manis" and extract_anim == True:
 				write_manis(archive, sized_str_entry)
-			elif sized_str_entry.ext == "fgm":
+			elif sized_str_entry.ext == "fgm" and extract_shader == True:
 				write_fgm(archive, sized_str_entry)
-			elif sized_str_entry.ext == "ms2":
+			elif sized_str_entry.ext == "ms2" and extract_model == True:
 				write_ms2(archive, sized_str_entry)
-			elif sized_str_entry.ext == "materialcollection":
+			elif sized_str_entry.ext == "materialcollection" and extract_shader == True:
 				write_materialcollection(archive, sized_str_entry)
-			elif sized_str_entry.ext == "tex":
+			elif sized_str_entry.ext == "tex" and extract_tex == True:
 				write_dds(archive, sized_str_entry, show_dds)
-			elif sized_str_entry.ext == "lua":
+			elif sized_str_entry.ext == "lua" and extract_lua == True:
 				write_lua(archive, sized_str_entry)
-			elif sized_str_entry.ext == "assetpkg":
+			elif sized_str_entry.ext == "assetpkg" and extract_misc == True:
 				write_assetpkg(archive, sized_str_entry)
-			elif sized_str_entry.ext == "fdb":
+			elif sized_str_entry.ext == "fdb" and extract_fdb == True:
 				write_fdb(archive, sized_str_entry)
-			elif sized_str_entry.ext == "xmlconfig":
+			elif sized_str_entry.ext == "xmlconfig" and extract_misc == True:
 				write_xmlconfig(archive, sized_str_entry)
-			elif sized_str_entry.ext == "userinterfaceicondata":
+			elif sized_str_entry.ext == "userinterfaceicondata" and extract_misc == True:
 				write_userinterfaceicondata(archive, sized_str_entry)
-			elif sized_str_entry.ext == "txt":
+			elif sized_str_entry.ext == "txt" and extract_text == True:
 				write_txt(archive, sized_str_entry)
-			elif sized_str_entry.ext == "bnk":
+			elif sized_str_entry.ext == "bnk" and extract_aux == True:
 				write_bnk(archive, sized_str_entry, show_dds, progress_callback)
-			elif sized_str_entry.ext == "prefab":
+			elif sized_str_entry.ext == "prefab" and extract_misc == True:
 				write_prefab(archive, sized_str_entry)
-			elif sized_str_entry.ext == "voxelskirt":
+			elif sized_str_entry.ext == "voxelskirt" and extract_misc == True:
 				write_voxelskirt(archive, sized_str_entry)
-			elif sized_str_entry.ext == "gfx":
+			elif sized_str_entry.ext == "gfx" and extract_misc == True:
 				write_gfx(archive, sized_str_entry)
-			elif sized_str_entry.ext == "fct":
+			elif sized_str_entry.ext == "fct" and extract_fct == True:
 				write_fct(archive, sized_str_entry)
-			elif sized_str_entry.ext == "scaleformlanguagedata":
+			elif sized_str_entry.ext == "scaleformlanguagedata" and extract_misc == True:
 				write_scaleform(archive, sized_str_entry)
 			else:
 				print("\nSkipping",sized_str_entry.name)
