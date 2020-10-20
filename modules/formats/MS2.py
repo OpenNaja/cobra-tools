e1c957cc6cbaa0c35847109140a89c56627d0616
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-10-20 15:39:47 +0200

    Don't dump all ms2 buffers

--- modules/formats/MS2.py
+++ modules/formats/MS2.py
@@ -30,10 +30,10 @@ def write_ms2(archive, ms2_sized_str_entry, out_dir):
 
 	print("\nWriting",name)
 	print("\nbuffers",len(buffers))
-	for i, buffer in enumerate(buffers):
-		p = out_dir(name+str(i)+".ms2")
-		with open(p, 'wb') as outfile:
-			outfile.write(buffer)
+	# for i, buffer in enumerate(buffers):
+	# 	p = out_dir(name+str(i)+".ms2")
+	# 	with open(p, 'wb') as outfile:
+	# 		outfile.write(buffer)
 
 	# Planet coaster
 	if archive.is_pc():
