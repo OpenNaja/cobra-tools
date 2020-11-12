884d5e2bf7c9d94122944b7cd47f6a0a89ac0894
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-11-12 17:08:39 +0100

    Fix PZ model export

--- generated/formats/ms2/compound/PzBone.py
+++ generated/formats/ms2/compound/PzBone.py
@@ -42,3 +42,10 @@ class PzBone:
 		s += '\n	* scale = ' + self.scale.__repr__()
 		s += '\n'
 		return s
+
+	def set_bone(self, matrix):
+		pos, quat, sca = matrix.decompose()
+		self.loc.x, self.loc.y, self.loc.z = -1 * pos.y, pos.z, pos.x
+		self.rot.x, self.rot.y, self.rot.z, self.rot.w = quat.y, -1 * quat.z, -1 * quat.x, quat.w
+		self.scale = sca.x
+
