b26694e5166a8f438de026edb722866f70fbb991
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-01-04 14:35:44 +0100

    Clean up djb function

--- modules/formats/shared.py
+++ modules/formats/shared.py
@@ -32,7 +32,7 @@ def get_padding(size, alignment=16):
 def djb(s):
 	# calculates DJB hash for string s
 	# from https://gist.github.com/mengzhuo/180cd6be8ba9e2743753#file-hash_djb2-py
-	hash = 5381
+	n = 5381
 	for x in s:
-		hash = ((hash << 5) + hash) + ord(x)
-	return hash & 0xFFFFFFFF
+		n = ((n << 5) + n) + ord(x)
+	return n & 0xFFFFFFFF
