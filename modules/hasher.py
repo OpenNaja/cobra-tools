d721f94c6a60741244faf6c42cfb399eb04eaf4f
harlequinzeg0 <j.wardftw@gmail.com>
2021-03-14 07:57:19 -0500

    Update hasher.py

--- modules/hasher.py
+++ modules/hasher.py
@@ -48,6 +48,7 @@ def dat_hasher(ovl, name_tups):
 				new_name, new_hash = old_hash_to_new[entry.file_hash]
 				entry.file_hash = new_hash
 				entry.name = f"{new_name}{entry.ext}"
+				print(entry.ext)
 			else:
 				new_name, new_hash = old_hash_to_new_pz[entry.file_hash]
 				#entry.file_hash = new_hash
@@ -102,6 +103,117 @@ def dat_hasher(ovl, name_tups):
 	#	print(err)
 	print("Done!")
     
+def dat_hasher_species(ovl, name_tups):
+	print(f"Hashing and Renaming for {name_tups}")
+	ovl_lists = [ovl.files, ovl.dependencies, ovl.dirs]
+	ovs_lists = []
+	for archive_entry in ovl.archives:
+		content = archive_entry.content
+		ovs_lists.extend((
+			content.data_entries,
+			content.set_header.sets,
+			content.set_header.assets,
+			content.header_entries,
+			content.sized_str_entries
+			))
+	old_hash_to_new = {}
+	old_hash_to_new_pz = {}
+	# first go over the ovl lists to generate new hashes
+	for i, entry_list in enumerate(ovl_lists):
+		for e,entry in enumerate(entry_list):
+			try:
+				if "bad hash" in entry.name:
+					print("Skipping", entry.name, entry.file_hash)
+					continue
+				if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
+					print("Skipping", entry.name, entry.file_hash)
+					continue
+				new_name = entry.name
+				for old, new in name_tups:
+					new_name = new_name.replace(old, new)
+				if hasattr(entry, "file_hash"):
+					new_hash = djb(new_name)
+					old_hash_to_new[entry.file_hash] = (new_name, new_hash)
+					old_hash_to_new_pz[e] = (new_name, new_hash)
+					print(f"List{i} {entry.name} -> {new_name},  {entry.file_hash} ->  {new_hash}")
+					entry.file_hash = new_hash
+				else:
+					print(f"List{i} {entry.name} -> {new_name},  [NOT HASHED]")
+				entry.name = new_name
+			except Exception as err:
+				print(err)
+
+	# we do this in a second step to resolve the links
+	for i, entry_list in enumerate(ovs_lists):
+		for entry in entry_list:
+			if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
+				print("Skipping", entry.name, entry.file_hash)
+				continue
+			if ovl.user_version.is_jwe:
+				new_name, new_hash = old_hash_to_new[entry.file_hash]
+				entry.file_hash = new_hash
+				entry.name = f"{new_name}{entry.ext}"
+			else:
+				new_name, new_hash = old_hash_to_new_pz[entry.file_hash]
+				#entry.file_hash = new_hash
+				entry.name = f"{new_name}{entry.ext}"
+				
+	# update the name buffer and offsets
+	ovl.names.update_with((
+		(ovl.dependencies, "ext"),
+		(ovl.dirs, "name"),
+		(ovl.mimes, "name"),
+		(ovl.files, "name")
+	))
+	ovl.len_names = len(ovl.names.data)
+	# resort the file entries
+	for i, file in enumerate(ovl.files):
+		file.old_index = i
+
+	# sort the different lists according to the criteria specified
+	ovl.files.sort(key=lambda x: (x.ext, x.file_hash))
+	ovl.dependencies.sort(key=lambda x: x.file_hash)
+
+	# create a lookup table to map the old indices to the new ones
+	lut = {}
+	for i, file in enumerate(ovl.files):
+		lut[file.old_index] = i
+
+	# update the file indices
+	for dependency in ovl.dependencies:
+		dependency.file_index = lut[dependency.file_index]
+	for aux in ovl.aux_entries:
+		aux.file_index = lut[aux.file_index]
+	if ovl.user_version.is_jwe:
+		print("JWE")
+	else:
+		for i, entry_list in enumerate(ovs_lists):
+			for entry in entry_list:
+				entry.file_hash = lut[entry.file_hash]
+                
+#	if ovl.user_version.is_jwe:
+	#	for old, new in name_tups:
+	#		old_c = old
+	#		new_c = new
+	#		old_ch = hex(djb(old_c.lower()))
+	#		new_ch = hex(djb(new_c.lower()))
+	#	name_tups_new = [(old_ch,new_ch),]
+ #       
+#	try:
+#		# hash the internal buffers
+#		for archive_entry in ovl.archives:
+#			ovs = archive_entry.content
+#			for header_entry in ovs.header_entries:
+#				b = header_entry.data.getvalue()
+#				header_entry.data = io.BytesIO(replace_bytes(b, name_tups_new))
+#			ovs.populate_pointers()
+#			for buffer_entry in ovs.buffer_entries:
+#				b = buffer_entry.data
+#				buffer_entry.data = replace_bytes(b, name_tups_new)
+#	except Exception as err:
+#		print(err)
+	print("Done!")
+    
     
 def dat_replacer(ovl, name_tups):
 	print(f"Replacing Dat contents for {name_tups}")
@@ -133,8 +245,8 @@ def species_dat_replacer(ovl, name_tups):
 			new_c = new
 			old_ch = hex(djb(old_c.lower()))
 			new_ch = hex(djb(new_c.lower()))
-			print(old_ch)
-		name_tups_new = [(old_a,new_a),(old_b,new_b),(old_c,new_c),(old_ch,new_ch)]
+		name_tups_new = [(old_a,new_a),(old_b,new_b),(old_ch,new_ch)]
+		name_tups_new2 = [(old_a,new_a),(old_b,new_b),(old_c,new_c),(old_ch,new_ch)]
 	else:
 		for old, new in name_tups:
 			old_a = old+"@"
@@ -519,8 +631,12 @@ def species_dat_replacer(ovl, name_tups):
 				header_entry.data = io.BytesIO(replace_bytes(b, name_tups_new))
 			ovs.populate_pointers()
 			for buffer_entry in ovs.buffer_entries:
-				b = buffer_entry.data
-				buffer_entry.data = replace_bytes(b, name_tups_new)
+				if ovl.user_version.is_jwe:
+					b = buffer_entry.data
+					buffer_entry.data = replace_bytes(b, name_tups_new2)
+				else:
+					b = buffer_entry.data
+					buffer_entry.data = replace_bytes(b, name_tups_new)
 	except Exception as err:
 		print(err)
 	print("Done!")
