cb024bb0346b5fcf04ecbe8548ebc9b3613375d5
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-03-24 07:08:42 +0100

    Refactor hasher species mode to use same code path

--- modules/hasher.py
+++ modules/hasher.py
@@ -1,10 +1,8 @@
-import os
 from modules.formats.shared import djb
 import io
-import struct
 
 
-def dat_hasher(ovl, name_tups):
+def dat_hasher(ovl, name_tups, species_mode=False):
 	print(f"Hashing and Renaming for {name_tups}")
 	ovl_lists = [ovl.files, ovl.dependencies, ovl.dirs]
 	ovs_lists = []
@@ -26,6 +24,10 @@ def dat_hasher(ovl, name_tups):
 				if "bad hash" in entry.name:
 					print("Skipping", entry.name, entry.file_hash)
 					continue
+				if species_mode:
+					if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
+						print("Skipping", entry.name, entry.file_hash)
+						continue
 				new_name = entry.basename
 				for old, new in name_tups:
 					new_name = new_name.replace(old, new)
@@ -44,6 +46,10 @@ def dat_hasher(ovl, name_tups):
 	# we do this in a second step to resolve the links
 	for i, entry_list in enumerate(ovs_lists):
 		for entry in entry_list:
+			if species_mode:
+				if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
+					print("Skipping", entry.name, entry.file_hash)
+					continue
 			if ovl.user_version.is_jwe:
 				new_name, new_hash = old_hash_to_new[entry.file_hash]
 				entry.file_hash = new_hash
@@ -81,92 +87,7 @@ def dat_hasher(ovl, name_tups):
 
 
 def dat_hasher_species(ovl, name_tups):
-	print(f"Hashing and Renaming for {name_tups}")
-	ovl_lists = [ovl.files, ovl.dependencies, ovl.dirs]
-	ovs_lists = []
-	for archive_entry in ovl.archives:
-		content = archive_entry.content
-		ovs_lists.extend((
-			content.data_entries,
-			content.set_header.sets,
-			content.set_header.assets,
-			content.header_entries,
-			content.sized_str_entries
-		))
-	old_hash_to_new = {}
-	old_hash_to_new_pz = {}
-	# first go over the ovl lists to generate new hashes
-	for i, entry_list in enumerate(ovl_lists):
-		for e, entry in enumerate(entry_list):
-			try:
-				if "bad hash" in entry.name:
-					print("Skipping", entry.name, entry.file_hash)
-					continue
-				if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
-					print("Skipping", entry.name, entry.file_hash)
-					continue
-				new_name = entry.name
-				for old, new in name_tups:
-					new_name = new_name.replace(old, new)
-				if hasattr(entry, "file_hash"):
-					new_hash = djb(new_name)
-					old_hash_to_new[entry.file_hash] = (new_name, new_hash)
-					old_hash_to_new_pz[e] = (new_name, new_hash)
-					print(f"List{i} {entry.name} -> {new_name},  {entry.file_hash} ->  {new_hash}")
-					entry.file_hash = new_hash
-				else:
-					print(f"List{i} {entry.name} -> {new_name},  [NOT HASHED]")
-				entry.name = new_name
-			except Exception as err:
-				print(err)
-
-	# we do this in a second step to resolve the links
-	for i, entry_list in enumerate(ovs_lists):
-		for entry in entry_list:
-			if entry.ext not in ".mdl2.ms2.motiongraph.materialcollection":
-				print("Skipping", entry.name, entry.file_hash)
-				continue
-			if ovl.user_version.is_jwe:
-				new_name, new_hash = old_hash_to_new[entry.file_hash]
-				entry.file_hash = new_hash
-			else:
-				new_name, new_hash = old_hash_to_new_pz[entry.file_hash]
-			entry.name = f"{new_name}{entry.ext}"
-
-	# update the name buffer and offsets
-	ovl.names.update_with((
-		(ovl.dependencies, "ext"),
-		(ovl.dirs, "name"),
-		(ovl.mimes, "name"),
-		(ovl.files, "name")
-	))
-	ovl.len_names = len(ovl.names.data)
-	# resort the file entries
-	for i, file in enumerate(ovl.files):
-		file.old_index = i
-
-	# sort the different lists according to the criteria specified
-	ovl.files.sort(key=lambda x: (x.ext, x.file_hash))
-	ovl.dependencies.sort(key=lambda x: x.file_hash)
-
-	# create a lookup table to map the old indices to the new ones
-	lut = {}
-	for i, file in enumerate(ovl.files):
-		lut[file.old_index] = i
-
-	# update the file indices
-	for dependency in ovl.dependencies:
-		dependency.file_index = lut[dependency.file_index]
-	for aux in ovl.aux_entries:
-		aux.file_index = lut[aux.file_index]
-	if ovl.user_version.is_jwe:
-		print("JWE")
-	else:
-		for i, entry_list in enumerate(ovs_lists):
-			for entry in entry_list:
-				entry.file_hash = lut[entry.file_hash]
-
-	print("Done!")
+	dat_hasher(ovl, name_tups, species_mode=True)
 
 
 def dat_replacer(ovl, name_tups):
