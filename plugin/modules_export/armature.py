bbc9e93a0ddfc7e423b2bb9b72ac3b60b6616d40
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-04-08 17:38:49 +0200

    Update bone enum fix

--- plugin/modules_export/armature.py
+++ plugin/modules_export/armature.py
@@ -101,7 +101,8 @@ def export_bones_custom(b_armature_ob, data):
 		bone_info.inverse_bind_matrices.append(ms2_inv_bind)
 
 	# update counts
-	bone_info.bind_matrix_count = bone_info.bone_count = bone_info.name_count = bone_info.bone_parents_count = len(b_bone_names)
+	bone_info.bind_matrix_count = bone_info.bone_count = bone_info.name_count = bone_info.bone_parents_count = \
+		bone_info.enum_count = len(b_bone_names)
 	bone_info.name_indices.resize(len(b_bone_names))
 	# paddings are taken care of automatically during writing
 	bone_info.enumeration.resize((len(b_bone_names), 2))
