cec7bf9a5251da8578d94aa5f28762ede749f154
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-07-04 18:49:54 +0200

    Improve manis

--- generated/formats/manis/compound/ManiBlock.py
+++ generated/formats/manis/compound/ManiBlock.py
@@ -23,10 +23,10 @@ class ManiBlock:
 		self.p_indices_00 = numpy.zeros((), dtype='ubyte')
 		self.p_indices_0 = numpy.zeros((), dtype='ubyte')
 		self.p_indices_0 = numpy.zeros((), dtype='ubyte')
-		self.p_indices_1 = numpy.zeros((), dtype='ubyte')
-		self.p_indices_1 = numpy.zeros((), dtype='ubyte')
-		self.p_indices_2 = numpy.zeros((), dtype='ubyte')
-		self.p_indices_2 = numpy.zeros((), dtype='ubyte')
+		self.p_indices_0_b = numpy.zeros((), dtype='ubyte')
+		self.p_indices_0_b = numpy.zeros((), dtype='ubyte')
+		self.p_indices_0_c = numpy.zeros((), dtype='ubyte')
+		self.p_indices_0_c = numpy.zeros((), dtype='ubyte')
 
 	def read(self, stream):
 
@@ -53,12 +53,12 @@ class ManiBlock:
 		self.p_indices_0 = stream.read_ubytes((self.arg.c))
 		if stream.version == 18:
 			self.p_indices_0 = stream.read_ubytes((self.arg.c))
-		self.p_indices_1 = stream.read_ubytes((self.arg.name_count))
+		self.p_indices_0_b = stream.read_ubytes((self.arg.ffff))
 		if stream.version == 18:
-			self.p_indices_1 = stream.read_ubytes((self.arg.name_count))
-		self.p_indices_2 = stream.read_ubytes((self.arg.e))
+			self.p_indices_0_b = stream.read_ubytes((self.arg.ffff))
+		self.p_indices_0_c = stream.read_ubytes((self.arg.ffff))
 		if stream.version == 18:
-			self.p_indices_2 = stream.read_ubytes((self.arg.e))
+			self.p_indices_0_c = stream.read_ubytes((self.arg.ffff))
 
 		self.io_size = stream.tell() - self.io_start
 
@@ -87,12 +87,12 @@ class ManiBlock:
 		stream.write_ubytes(self.p_indices_0)
 		if stream.version == 18:
 			stream.write_ubytes(self.p_indices_0)
-		stream.write_ubytes(self.p_indices_1)
+		stream.write_ubytes(self.p_indices_0_b)
 		if stream.version == 18:
-			stream.write_ubytes(self.p_indices_1)
-		stream.write_ubytes(self.p_indices_2)
+			stream.write_ubytes(self.p_indices_0_b)
+		stream.write_ubytes(self.p_indices_0_c)
 		if stream.version == 18:
-			stream.write_ubytes(self.p_indices_2)
+			stream.write_ubytes(self.p_indices_0_c)
 
 		self.io_size = stream.tell() - self.io_start
 
@@ -107,8 +107,8 @@ class ManiBlock:
 		s += f'\n	* indices_e_2 = {self.indices_e_2.__repr__()}'
 		s += f'\n	* p_indices_00 = {self.p_indices_00.__repr__()}'
 		s += f'\n	* p_indices_0 = {self.p_indices_0.__repr__()}'
-		s += f'\n	* p_indices_1 = {self.p_indices_1.__repr__()}'
-		s += f'\n	* p_indices_2 = {self.p_indices_2.__repr__()}'
+		s += f'\n	* p_indices_0_b = {self.p_indices_0_b.__repr__()}'
+		s += f'\n	* p_indices_0_c = {self.p_indices_0_c.__repr__()}'
 		return s
 
 	def __repr__(self):
