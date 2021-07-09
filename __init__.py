016250fe205d7e6e422b90ca0edcc4f58ce756dd
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-07-09 12:58:20 +0200

    Increment version number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {	"name": "Frontier's Cobra Engine Formats (JWE, Planet Zoo)",
 			"author": "Harlequinz Ego & HENDRIX",
 			"blender": (2, 92, 0),
-			"version": (2, 2, 8),
+			"version": (2, 3, 0),
 			"location": "File > Import-Export",
 			"description": "Import-Export models, skeletons and animations.",
 			"warning": "",
