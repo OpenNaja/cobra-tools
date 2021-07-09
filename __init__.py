d77f0a34a56c910b59c6e1245ded522e839971cc
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-07-09 11:46:11 +0200

    Add auto addon updater

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 bl_info = {	"name": "Frontier's Cobra Engine Formats (JWE, Planet Zoo)",
 			"author": "Harlequinz Ego & HENDRIX",
 			"blender": (2, 92, 0),
+			"version": (2, 2, 8),
 			"location": "File > Import-Export",
 			"description": "Import-Export models, skeletons and animations.",
 			"warning": "",
@@ -13,6 +14,7 @@ import bpy
 import bpy.utils.previews
 from bpy.props import StringProperty, BoolProperty, CollectionProperty
 from bpy_extras.io_utils import ImportHelper, ExportHelper
+from . import addon_updater_ops
 
 import os
 import sys
@@ -39,6 +41,68 @@ def handle_errors(inst, func, kwargs):
 		inst.report({"ERROR"}, str(err))
 		traceback.print_exc()
 	return {'FINISHED'}
+
+
+@addon_updater_ops.make_annotations
+class CobraPreferences(bpy.types.AddonPreferences):
+	"""Cobra preferences"""
+	bl_idname = __package__
+
+	# Addon updater preferences.
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False)
+
+	updater_interval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0)
+
+	updater_interval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31)
+
+	updater_interval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23)
+
+	updater_interval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59)
+
+	def draw(self, context):
+		layout = self.layout
+
+		# Works best if a column, or even just self.layout.
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# Updater draw function, could also pass in col as third arg.
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# ops = col.operator("wm.url_open","Open webpage ")
+		# ops.url=addon_updater_ops.updater.website
 
 
 class ImportBani(bpy.types.Operator, ImportHelper):
@@ -194,6 +258,7 @@ class MESH_PT_CobraTools(bpy.types.Panel):
 		row.operator("object.vcol_to_comb", icon_value=preview_collection["frontier.png"].icon_id)
 		sub = row.row()
 		sub.operator("object.comb_to_vcol", icon_value=preview_collection["frontier.png"].icon_id)
+		addon_updater_ops.update_notice_box_ui()
 
 
 def menu_func_export(self, context):
@@ -219,11 +284,13 @@ classes = (
 	GaugeUVScale,
 	VcolToHair,
 	HairToVcol,
-	MESH_PT_CobraTools
+	MESH_PT_CobraTools,
+	CobraPreferences
 	)
 
 
 def register():
+	addon_updater_ops.register(bl_info)
 	import os
 	icons_dir = os.path.join(os.path.dirname(__file__), "icons")
 	for icon_name_ext in os.listdir(icons_dir):
@@ -232,7 +299,6 @@ def register():
 
 	for cls in classes:
 		bpy.utils.register_class(cls)
-	
 	bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 	bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 	# bpy.types.VIEW3D_PT_tools_object.append(menu_func_object)
