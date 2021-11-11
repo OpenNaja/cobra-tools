d7906e1429e0c5936db4d401e60f29d58c0aab6b
harlequinzeg0 <j.wardftw@gmail.com>
2021-11-10 19:02:20 -0600

    Update MATLAYERS.py

--- modules/formats/MATLAYERS.py
+++ modules/formats/MATLAYERS.py
@@ -121,10 +121,9 @@ class MatlayersLoader(BaseFile):
 		# 0,0,collection count,0, 0,0,
 		# print(self.sized_str_entry.f1.pointers[0].data, len(self.sized_str_entry.f1.pointers[0].data))
 		f0_d0 = struct.unpack("<6I", self.sized_str_entry.f1.pointers[0].data)
-		layer_count = f0_d0[2]
+		layer_count = f0_d0[2] - 1
 		print(f0_d0)
-		self.sized_str_entry.tex_frags = self.ovs.frags_from_pointer(self.sized_str_entry.f1.pointers[1],
-																	 layer_count*2)
+		self.sized_str_entry.tex_frags = self.ovs.frags_accumulate_from_pointer(self.sized_str_entry.f1.pointers[1], (16 + (layer_count*24)))
 		for tex in self.sized_str_entry.tex_frags:
 			# p0 is just 1 or 0, but weird since 8 and 16 bytes alternate
 			# first is fgm name, second layer identity name
