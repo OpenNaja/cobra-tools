43cfe7801a78e394f74007ed2ee9efc4df9c20ab
ilodev <ilodev@users.noreply.github.com>
2021-11-18 02:16:41 -0800

    working on adding specdefs to the ovl
    at this stage it creates just empty specdefs, works on jwe1

--- modules/formats/SPECDEF.py
+++ modules/formats/SPECDEF.py
@@ -6,6 +6,52 @@ import struct
 
 class SpecdefLoader(BaseFile):
 
+	def _get_data(self, file_path):
+		"""Loads and returns the data for a specdef"""
+		buffer_0 = self.get_content(file_path)
+		ss = struct.pack("IIII", len(buffer_0), 16000, 0x00, 0x00)
+		return ss, buffer_0
+
+	def create(self):
+		# ignore content, just write an empty specdef 
+		ss, buffer_0 = self._get_data(self.file_entry.path)
+		file_name_bytes = self.file_entry.basename.encode(encoding='utf8')
+		pool_index, pool = self.get_pool(2)
+		offset = pool.data.tell()
+		print(f"pool type: {pool.type}")
+		dpool_index, dpool = self.get_pool(2)
+		doffset = dpool.data.tell()
+
+		# add empty specdef, 64b size struct
+		empty_buffer  = struct.pack("<64s", b'')
+		pool.data.write(empty_buffer) 
+		empty_data    = struct.pack("<I4s", 0x87126e, b'') 
+		dpool.data.write(empty_data)  
+
+		# add three required fragments for the specdef
+		# ignoring the current specdef struct, point all
+		# fragments to the beginning of the buffer
+		new_frag0 = self.create_fragment()
+		new_frag0.pointers[0].pool_index = pool_index
+		new_frag0.pointers[0].data_offset = offset + 0x08
+		new_frag0.pointers[1].pool_index = dpool_index
+		new_frag0.pointers[1].data_offset = doffset + 0x00
+		new_frag1 = self.create_fragment()
+		new_frag1.pointers[0].pool_index = pool_index
+		new_frag1.pointers[0].data_offset = offset + 0x10
+		new_frag1.pointers[1].pool_index = dpool_index
+		new_frag1.pointers[1].data_offset = doffset + 0x00
+		new_frag2 = self.create_fragment()
+		new_frag2.pointers[0].pool_index = pool_index
+		new_frag2.pointers[0].data_offset = offset + 0x18
+		new_frag2.pointers[1].pool_index = dpool_index
+		new_frag2.pointers[1].data_offset = doffset + 0x00
+
+		self.sized_str_entry = self.create_ss_entry(self.file_entry)
+		self.sized_str_entry.pointers[0].pool_index = pool_index
+		self.sized_str_entry.pointers[0].data_offset = offset
+		self.create_data_entry(self.sized_str_entry, (empty_buffer,))
+
 	def collect(self):
 		self.assign_ss_entry()
 		ss_pointer = self.sized_str_entry.pointers[0]
@@ -15,6 +61,7 @@ class SpecdefLoader(BaseFile):
 		if ss_data[0] == 0:
 			logging.info(f"spec is zero ")
 		self.sized_str_entry.fragments = self.ovs.frags_from_pointer(ss_pointer, 3, reuse=False)
+		logging.info(f"SPECDEF fragments: {self.sized_str_entry.fragments}")
 		attrib_count = ss_data[0]
 		conditions = ss_data[2:]
 		self.condition_frags = []
@@ -45,10 +92,15 @@ class SpecdefLoader(BaseFile):
 
 		# save .bin data
 		with open(out_path + ".bin", 'wb') as outfile:
-			logging.debug("Exporting binary specdef file")
+			logging.warning("Exporting binary specdef file")
+			logging.warning(f"SPECDEF: {self.sized_str_entry.pointers}")
+			logging.warning(f"SPECDEF: {self.sized_str_entry.fragments}")
+			logging.warning(f"SPECDEF: {self.sized_str_entry.pointers[0].data}")
 			outfile.write(ovl_header)
 			outfile.write(self.sized_str_entry.pointers[0].data)
 			for f in self.sized_str_entry.fragments:
+				logging.warning(f"SPECDEF: dumping pool type {f}")
+
 				outfile.write(f.pointers[1].data)
 			outfile.close()
 
@@ -122,3 +174,4 @@ class SpecdefLoader(BaseFile):
 						outfile.write(outstr + "\n")
 
 		return out_path + ".bin", out_path,
+
