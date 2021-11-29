208100edbd65668b215672b9800dba79ec6136bc
ilodev <ilodev@users.noreply.github.com>
2021-11-29 09:21:30 -0800

    Preparing module for MergeDetails mime

---
+++ modules/formats/MERGEDETAILS.py
@@ -0,0 +1,19 @@
+import struct
+
+from modules.formats.BaseFormat import BaseFile
+import xml.etree.ElementTree as ET # prob move this to a custom modules.helpers or utils?
+
+
+class MergeDetailsLoader(BaseFile):
+
+	def create(self):
+		pass
+
+	def collect(self):
+		pass
+
+	def load(self, file_path):
+		pass
+
+	def extract(self, out_dir, show_temp_files, progress_callback):
+		pass
