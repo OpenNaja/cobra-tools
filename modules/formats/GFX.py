31b07520200291a7e6e99e8ed36cd556e0f554e2
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-11-29 17:06:47 +0100

    Fix GFX

--- modules/formats/GFX.py
+++ modules/formats/GFX.py
@@ -3,28 +3,19 @@ import struct
 
 from modules.formats.BaseFormat import BaseFile
 
+
 class GfxLoader(BaseFile):
 
     def create(self):
         ss, buffer_0 = self._get_data(self.file_entry.path)
-        file_name_bytes = self.file_entry.basename.encode(encoding='utf8')
-        pool_index, pool = self.get_pool(2)
-        offset = pool.data.tell()
-        # lua, ss, 2 frag + buffer
-        pool.data.write(ss)  # ss data
-
         self.sized_str_entry = self.create_ss_entry(self.file_entry)
-        self.sized_str_entry.pointers[0].pool_index = pool_index
-        self.sized_str_entry.pointers[0].data_offset = offset
+        self.write_to_pool(self.sized_str_entry.pointers[0], 4, ss)
         self.create_data_entry(self.sized_str_entry, (buffer_0,))
 
-
     def load(self, file_path):
-        # all meta data of the lua except the sized str entries gfx size value seems to just be meta data, can be zeroed
         ss, buffer_0 = self._get_data(file_path)
         self.sized_str_entry.data_entry.update_data((buffer_0,))
         self.sized_str_entry.pointers[0].update_data(ss, update_copies=True)
-
 
     def collect(self):
         self.assign_ss_entry()
@@ -43,5 +34,5 @@ class GfxLoader(BaseFile):
     def _get_data(self, file_path):
         """Loads and returns the data for a GFX"""
         buffer_0 = self.get_content(file_path)
-        ss = struct.pack("<QQQQ", 0, len(buffer_0), 0,0)
+        ss = struct.pack("<QQQQ", 0, len(buffer_0), 0, 0)
         return ss, buffer_0
