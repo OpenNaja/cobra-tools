11fb929c0bf9be452aae70ea9bcc6c9470b2bd68
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-12-05 12:13:13 +0100

    Create XMLCONFIG

--- modules/formats/XMLCONFIG.py
+++ modules/formats/XMLCONFIG.py
@@ -1,3 +1,4 @@
+import logging
 
 from modules.formats.BaseFormat import BaseFile
 from modules.formats.shared import get_padding
@@ -6,19 +7,26 @@ from modules.helpers import zstr
 
 class XmlconfigLoader(BaseFile):
 
+	def create(self):
+		data = self._get_data(self.file_entry.path)
+		self.sized_str_entry = self.create_ss_entry(self.file_entry)
+		f_0 = self.create_fragments(self.sized_str_entry, 1)[0]
+		self.write_to_pool(f_0.pointers[1], 2, data)
+		self.write_to_pool(self.sized_str_entry.pointers[0], 2, b'\x00' * 8)
+		self.write_to_pool(f_0.pointers[0], 2, b'\x00' * 8)
+
 	def collect(self):
 		self.assign_ss_entry()
 		self.assign_fixed_frags(1)
 
 	def extract(self, out_dir, show_temp_files, progress_callback):
 		name = self.sized_str_entry.name
-		print("\nWriting", name)
+		logging.info(f"Writing {name}")
 
 		if len(self.sized_str_entry.fragments) == 1:
 			f_0 = self.sized_str_entry.fragments[0]
 		else:
-			print("Found wrong amount of frags for", name)
-			return
+			raise AttributeError(f"Found wrong amount of frags [{len(self.sized_str_entry.fragments)}] for {name}")
 		# write xml
 		out_path = out_dir(name)
 		with open(out_path, 'wb') as outfile:
@@ -34,8 +42,11 @@ class XmlconfigLoader(BaseFile):
 		return out_path,
 
 	def load(self, file_path):
+		data = self._get_data(file_path)
+		self.sized_str_entry.fragments[0].pointers[1].update_data(data, update_copies=True)
+
+	def _get_data(self, file_path):
 		with open(file_path, 'rb') as stream:
 			# add zero terminator
 			data = stream.read() + b"\x00"
-			# make sure all are updated, and pad to 8 bytes
-			self.sized_str_entry.fragments[0].pointers[1].update_data(data, update_copies=True, pad_to=8)
+			return data + get_padding(len(data), 8)
