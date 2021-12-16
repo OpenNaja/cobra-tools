87db14947e63f55a0e72e3c70028d1a18c68ac34
HENDRIX-ZT2 <arnfinn@spholderer.de>
2021-12-16 19:43:31 +0100

    Refactor pscollection

--- modules/formats/PSCOLLECTION.py
+++ modules/formats/PSCOLLECTION.py
@@ -1,18 +1,17 @@
+import logging
 import struct
 
 from modules.formats.BaseFormat import BaseFile
-import xml.etree.ElementTree as ET # prob move this to a custom modules.helpers or utils?
+import xml.etree.ElementTree as ET  # prob move this to a custom modules.helpers or utils?
+
+from modules.formats.shared import get_padding
 
 
 class PSCollectionLoader(BaseFile):
 
 	def create(self):
 		ss = self.get_content(self.file_entry.path)
-		pool_index, pool = self.get_pool(2)
-		offset = pool.data.tell()
 		self.sized_str_entry = self.create_ss_entry(self.file_entry)
-		self.sized_str_entry.pointers[0].pool_index = pool_index
-		self.sized_str_entry.pointers[0].data_offset = offset
 
 		# read all in a dict from the xml
 		psdata = ET.ElementTree(ET.fromstring(ss))
@@ -20,7 +19,7 @@ class PSCollectionLoader(BaseFile):
 		pslistdata = psdata.findall('.//PreparedStatement')
 		pslist = []
 		for psdata in pslistdata:
-			psentry = { 'name': psdata.attrib['name'], 'sql': psdata.attrib['sql'], 'args': [] }
+			psentry = {'name': psdata.attrib['name'], 'sql': psdata.attrib['sql'], 'args': []}
 
 			argdata = psdata.findall('.//ArgumentType')
 			psargs = []
@@ -29,94 +28,50 @@ class PSCollectionLoader(BaseFile):
 			psentry['args'] = psargs
 			pslist.append(psentry)
 
-		print(pslist)
-		pscount = len(pslist)
-
 		# pscollection needs 8 bytes for the ptr and the array count
 		# then also needs more per each entry and each arg
-		pool.data.write(struct.pack("<QQ", 0, pscount))  # room for 16 bytes
-
-		# new offset for list pointers
-		poffset = pool.data.tell()
-
-		# point the first frag points to the array of data now
-		new_frag0 = self.create_fragment()
-		new_frag0.pointers[0].pool_index = pool_index
-		new_frag0.pointers[0].data_offset = offset + 0x00
-		new_frag0.pointers[1].pool_index = pool_index
-		new_frag0.pointers[1].data_offset = poffset
-
-		psptr = pool.data.tell() # ptr to the array entry for this ps, used to create frags from now
+		f_0 = self.create_fragments(self.sized_str_entry, 1)[0]
+		self.write_to_pool(f_0.pointers[0], 2, b"")
+		self.write_to_pool(self.sized_str_entry.pointers[0], 2, struct.pack("<QQ", 0, len(pslist)))
+		# point the first frag to the array of data now
+		# ptr, count, ptr ptr
+		self.write_to_pool(f_0.pointers[1], 2, b"".join(struct.pack("<QQQQ", 0, len(ps['args']), 0, 0) for ps in pslist))
+		rel_offset = 0
 		for ps in pslist:
-			d = struct.pack("<QQQQ", 0, len(ps['args']), 0, 0 )
-			print(f"{d}")
-			pool.data.write(struct.pack("<QQQQ", 0, len(ps['args']), 0, 0 )) # ptr, count, ptr ptr
-
-		for ps in pslist:
-			pdptr = pool.data.tell() # ptr to the ps args
-
-			argcount = 1
-			for argtype in ps['args']:
-				d = struct.pack('<BBBBIQQ', 0, argtype, argcount, 0, 0, 0, 0 )
-				print(f"{d}")
-				pool.data.write(struct.pack('<BBBBIQQ', 0, argtype, argcount, 0, 0, 0, 0 )) 
-				argcount += 1
-
-			# fix possible padding
-			if argcount % 2 == 0:
-				d = struct.pack('<Q', 0 )
-				print(f"{d}")
-				pool.data.write(struct.pack('<Q', 0 )) 
-
 			# if there are args, make a frag for it
-			if len(ps['args']):
-				new_frag = self.create_fragment()
-				new_frag.pointers[0].pool_index = pool_index
-				new_frag.pointers[0].data_offset = psptr
-				new_frag.pointers[1].pool_index = pool_index
-				new_frag.pointers[1].data_offset = pdptr
+			if ps['args']:
+				f = self.create_fragments(self.sized_str_entry, 1)[0]
+				args_data = b"".join(struct.pack('<BBBBIQQ', 0, argtype, 1+i, 0, 0, 0, 0) for i, argtype in enumerate(ps['args']))
+				self.ptr_relative(f.pointers[0], f_0.pointers[1], rel_offset=rel_offset)
+				self.write_to_pool(f.pointers[1], 2, args_data + get_padding(args_data, alignment=16))
 
-			# write name and add ptr
-			nameptr = pool.data.tell() # 
-			pool.data.write(f"{ps['name']}\00".encode('utf-8'))
-			new_frag = self.create_fragment()
-			new_frag.pointers[0].pool_index = pool_index
-			new_frag.pointers[0].data_offset = psptr + 0x10
-			new_frag.pointers[1].pool_index = pool_index
-			new_frag.pointers[1].data_offset = nameptr
+			# write name and sql as a name ptr each
+			f_name, f_sql = self.create_fragments(self.sized_str_entry, 2)
+			self.ptr_relative(f_name.pointers[0], f_0.pointers[1], rel_offset=rel_offset + 0x10)
+			self.write_to_pool(f_name.pointers[1], 2, f"{ps['name']}\00".encode('utf-8'))
+			self.ptr_relative(f_sql.pointers[0], f_0.pointers[1], rel_offset=rel_offset + 0x18)
+			self.write_to_pool(f_sql.pointers[1], 2, f"{ps['sql']}\00".encode('utf-8'))
 
-
-			# write sql statement and ptr
-			sqlptr = pool.data.tell() # 
-			pool.data.write(f"{ps['sql']}\00".encode('utf-8'))
-			new_frag = self.create_fragment()
-			new_frag.pointers[0].pool_index = pool_index
-			new_frag.pointers[0].data_offset = psptr + 0x18
-			new_frag.pointers[1].pool_index = pool_index
-			new_frag.pointers[1].data_offset = sqlptr
-
-			#increase psptr to the next array member
-			psptr += 0x20
-
-		pass
+			# increase psptr to the next array member
+			rel_offset += 0x20
 
 	def collect(self):
 		self.assign_ss_entry()
-		print(f"Collecting {self.sized_str_entry.name}")
+		logging.info(f"Collecting {self.sized_str_entry.name}")
 
 		pscount = struct.unpack("<QQ", self.sized_str_entry.pointers[0].data)[1]
 		self.sized_str_entry.pscount = pscount
-		#print(f"prepared statements: {self.sized_str_entry.pscount}")
+		# print(f"prepared statements: {self.sized_str_entry.pscount}")
 
-		#get ptr to data array
+		# get ptr to data array
 		psfragment = self.ovs.frags_from_pointer(self.sized_str_entry.pointers[0], 1)[0]
-		psdata = psfragment.pointers[1].read_from_pool(0x20*pscount)
+		psdata = psfragment.pointers[1].read_from_pool(0x20 * pscount)
 		self.sized_str_entry.pslist = []
 		offset = 0
-		index  = 1
+		index = 1
 		for x in range(pscount):
-			_, argcount, _, _ = struct.unpack("<QQQQ", psdata[ offset : offset + 0x20])
-			#print(f"argcount: {argcount}")
+			_, argcount, _, _ = struct.unpack("<QQQQ", psdata[offset: offset + 0x20])
+			# print(f"argcount: {argcount}")
 
 			# if argcount get args
 			psargs = []
@@ -125,8 +80,9 @@ class PSCollectionLoader(BaseFile):
 				argsdata = argsfragment.pointers[1].read_from_pool(0x18 * argcount)
 				dataoffset = 0
 				for x in range(argcount):
-					_, argType, argindex, _, _, _, _ = struct.unpack('<BBBBIQQ', argsdata[ dataoffset : dataoffset+0x18 ] )
-					#print(f"argtype: {argType}  argindex: {argindex}")
+					_, argType, argindex, _, _, _, _ = struct.unpack('<BBBBIQQ',
+																	 argsdata[dataoffset: dataoffset + 0x18])
+					# print(f"argtype: {argType}  argindex: {argindex}")
 					psargs.append(int(argType))
 					dataoffset += 0x18
 
@@ -137,7 +93,7 @@ class PSCollectionLoader(BaseFile):
 			if strval[-1] == '\x00':
 				strval = strval[:-1]
 			name = strval
-			#print(name)
+			# print(name)
 
 			# get sql
 			sqlfragment = self.ovs.frags_from_pointer(psfragment.pointers[1], 1)[0]
@@ -146,16 +102,15 @@ class PSCollectionLoader(BaseFile):
 			if strval[-1] == '\x00':
 				strval = strval[:-1]
 			sqldata = strval
-			#print(sqldata)
-			#print("----")
+			# print(sqldata)
+			# print("----")
 			# update offset
 			offset += 0x20
 
-			psentry = { 'name': name, 'sql': sqldata, 'args': psargs }
-			self.sized_str_entry.pslist.append(psentry) 
+			psentry = {'name': name, 'sql': sqldata, 'args': psargs}
+			self.sized_str_entry.pslist.append(psentry)
 
-
-		#print(self.sized_str_entry.pslist)
+		# print(self.sized_str_entry.pslist)
 		pass
 
 	def load(self, file_path):
@@ -169,21 +124,20 @@ class PSCollectionLoader(BaseFile):
 		out_path = out_dir(name)
 		xmldata = ET.Element('PSCollection')
 
-		for ps in self.sized_str_entry.pslist: 
+		for ps in self.sized_str_entry.pslist:
 			psitem = ET.SubElement(xmldata, 'PreparedStatement')
 			psitem.set('name', str(ps['name']))
-			psitem.set('sql',  str(ps['sql']))
+			psitem.set('sql', str(ps['sql']))
 
 			if len(ps['args']):
 				for arg in ps['args']:
-				  argitem = ET.SubElement(psitem, 'ArgumentType')
-				  argitem.text = str(arg)
+					argitem = ET.SubElement(psitem, 'ArgumentType')
+					argitem.text = str(arg)
 
 		xmltext = ET.tostring(xmldata)
 
 		with open(out_path, 'w') as outfile:
 			outfile.write(xmltext.decode('utf-8'))
 			out_files.append(out_path)
-		    
-		return out_files
 
+		return out_files
