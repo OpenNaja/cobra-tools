55c68f03cf914fa39b4a842f294b33fcab0b8f96
ilodev <ilodev@users.noreply.github.com>
2021-12-30 04:49:13 -0800

    extract/inject/create for AnimationDataCurve

--- modules/formats/CURVE.py
+++ modules/formats/CURVE.py
@@ -3,13 +3,12 @@ import struct
 from modules.formats.shared import get_padding
 from modules.formats.BaseFormat import BaseFile
 from modules.helpers import zstr
+import xml.etree.ElementTree as ET # prob move this to a custom modules.helpers or utils?
 
 
 class CurveLoader(BaseFile):
 
 	def create(self):
-		if True:
-			pass
 		f_0, f_1 = self._get_data(self.file_entry.path)
 		self.sized_str_entry = self.create_ss_entry(self.file_entry)
 		f = self.create_fragments(self.sized_str_entry, 1)[0]
@@ -18,32 +17,50 @@ class CurveLoader(BaseFile):
 		self.write_to_pool(f.pointers[0], 4, f_0)
 
 	def collect(self):
-		if True:
-			pass
 		self.assign_ss_entry()
+		logging.info(f"Collecting {self.sized_str_entry.name}")
 		self.assign_fixed_frags(1)
 
+		_, count = struct.unpack("<QQ", self.sized_str_entry.pointers[0].data)
+		self.sized_str_entry.keyvalues = []
+		logging.info(f"count {count}")
+
+		# get ptr to data
+		cfragment = self.sized_str_entry.fragments[0]
+		cdata = cfragment.pointers[1].read_from_pool(0x8 * count)
+		offset = 0
+		for x in range(count):
+			key, value = struct.unpack("<ff", cdata[offset: offset + 0x08])
+			#logging.info(f"key:  {key}  value: {value}")
+			item = { 'key' : key, 'value' : value }
+			self.sized_str_entry.keyvalues.append(item)
+			offset += 0x08
+
 	def load(self, file_path):
-		if True:
-			pass
 		f_0, f_1 = self._get_data(file_path)
 		self.sized_str_entry.fragments[0].pointers[1].update_data(f_1, update_copies=True)
 
 	def extract(self, out_dir, show_temp_files, progress_callback):
-		if True:
-			pass
 		name = self.sized_str_entry.name
 		logging.info(f"Writing {name}")
-		f_0 = self.sized_str_entry.fragments[0]
+
+		xml_data = ET.Element('AnimationCurveData')
+		for item in self.sized_str_entry.keyvalues:
+			xmlitem = ET.SubElement(xml_data, 'KeyValue')
+			xmlitem.set('key', str(item['key']))
+			xmlitem.text = str(item['value'])
+
 		out_path = out_dir(name)
-		with open(out_path, 'wb') as outfile:
-			f_0.pointers[1].strip_zstring_padding()
-			outfile.write(f_0.pointers[1].data[:-1])
+		self.write_xml(out_path, xml_data)
 		return out_path,
 
 	def _get_data(self, file_path):
 		"""Loads and returns the data for a CURVE"""
-		# copy content, pad to 64b, then assign 1 fragment and 1 empty sized str.
-		f_0 = struct.pack('16s', b'')  # fragment pointer 0 data
-		f_1 = zstr(self.get_content(file_path))  # fragment pointer 1 data
-		return f_0, f_1 + get_padding(len(f_1), alignment=64)
+		curvedata = self.load_xml(file_path)
+		f_1 = bytearray()
+		for item in curvedata:
+			f_1 += struct.pack("<ff", float(item.attrib['key']), float(item.text))
+		f_0 = struct.pack('<QQ', 0x00, len(curvedata))  # fragment pointer 0 data
+
+		# there is not a reason to add this padding but just to be safe
+		return f_0, f_1 + get_padding(len(f_1), alignment=16)
