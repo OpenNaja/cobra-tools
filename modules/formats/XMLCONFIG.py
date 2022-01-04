2534c5b02cede6f734e4aab068ec16db98ece4c7
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-01-04 20:37:23 +0100

    Refactor XMLCONFIG.py

--- modules/formats/XMLCONFIG.py
+++ modules/formats/XMLCONFIG.py
@@ -10,8 +10,8 @@ class XmlconfigLoader(BaseFile):
 		self.sized_str_entry = self.create_ss_entry(self.file_entry)
 		f_0 = self.create_fragments(self.sized_str_entry, 1)[0]
 		self.write_to_pool(f_0.pointers[1], 2, data)
-		self.write_to_pool(self.sized_str_entry.pointers[0], 2, b'\x00' * 8)
-		self.write_to_pool(f_0.pointers[0], 2, b'\x00' * 8)
+		self.write_to_pool(self.sized_str_entry.pointers[0], 2, b'\x00' * 16)
+		self.ptr_relative(f_0.pointers[0], self.sized_str_entry.pointers[0], 8)
 
 	def collect(self):
 		self.assign_ss_entry()
@@ -20,19 +20,17 @@ class XmlconfigLoader(BaseFile):
 	def extract(self, out_dir, show_temp_files, progress_callback):
 		name = self.sized_str_entry.name
 		logging.info(f"Writing {name}")
-		f_0 = self.sized_str_entry.fragments[0]
 		out_path = out_dir(name)
 		with open(out_path, 'wb') as outfile:
-			f_0.pointers[1].strip_zstring_padding()
-			# strip the b00 zstr terminator byte
-			outfile.write(f_0.pointers[1].data[:-1])
+			outfile.write(self.p1_ztsr(self.sized_str_entry.fragments[0]))
 		return out_path,
 
 	def load(self, file_path):
 		data = self._get_data(file_path)
 		self.sized_str_entry.fragments[0].pointers[1].update_data(data, update_copies=True)
 
-	def _get_data(self, file_path):
+	@staticmethod
+	def _get_data(file_path):
 		with open(file_path, 'rb') as stream:
 			# add zero terminator
 			data = stream.read() + b"\x00"
