2e1f8fae55399665ab069d36e93bd3902878e593
ilodev <ilodev@users.noreply.github.com>
2022-01-18 09:49:56 -0800

    Improved UIMOVIEDEFINITION,
    create/extract working again

--- modules/formats/UIMOVIEDEFINITION.py
+++ modules/formats/UIMOVIEDEFINITION.py
@@ -14,7 +14,7 @@ class UIMovieDefinitionLoader(BaseFile):
 
 	def create(self):
 		self.sized_str_entry = self.create_ss_entry(self.file_entry)
-		print(f"Loading {self.file_entry.path}")
+
 		moviedef = self.load_xml(self.file_entry.path)
 
 		namelistdata = moviedef.findall('.//Name')
@@ -98,29 +98,21 @@ class UIMovieDefinitionLoader(BaseFile):
 			self.ptr_relative(new_frag1.pointers[1], item_frags[0].pointers[0])
 
 		if len(self.Count1List):
-			# for each line, add the frag ptr space and create the frag ptr
-			item_frag = self.create_fragments(self.sized_str_entry, 1)[0]
-			for item in self.Count1List:
-				print(f"item {item}")
-				self.write_to_pool(item_frag.pointers[1], 2, struct.pack("<I", int(item)))
-
-			# point the list frag to the end of the data now.
 			new_frag1 = self.create_fragments(self.sized_str_entry, 1)[0]
 			self.ptr_relative(new_frag1.pointers[0], self.sized_str_entry.pointers[0], 0x70)
-			self.ptr_relative(new_frag1.pointers[1], item_frag.pointers[0])
+			itembytes = b''
+			for item in self.Count1List:
+				itembytes += struct.pack("<I", int(item))
+			self.write_to_pool(new_frag1.pointers[1], 2, itembytes)
 
 		if len(self.Count2List):
-			# for each line, add the frag ptr space and create the frag ptr
-			item_frag = self.create_fragments(self.sized_str_entry, 1)[0]
-
-			for item in self.Count2List:
-				print(f"item {item}")
-				self.write_to_pool(item_frag.pointers[1], 2, struct.pack("<I", int(item)))
-
 			# point the list frag to the end of the data now.
 			new_frag1 = self.create_fragments(self.sized_str_entry, 1)[0]
 			self.ptr_relative(new_frag1.pointers[0], self.sized_str_entry.pointers[0], 0x78)
-			self.ptr_relative(new_frag1.pointers[1], item_frag.pointers[0])
+			itembytes = b''
+			for item in self.Count2List:
+				itembytes += struct.pack("<I", int(item))
+			self.write_to_pool(new_frag1.pointers[1], 2, itembytes)
 
 
 		if len(self.uiInterfacelist):
@@ -138,212 +130,6 @@ class UIMovieDefinitionLoader(BaseFile):
 			self.ptr_relative(new_frag1.pointers[1], item_frags[0].pointers[0])
 
 
-
-
-
-
-
-
-
-
-
-
-
-		if 1 == 1:
-			return
-
-		ss = self.get_content(self.file_entry.path)
-		pool = self.get_pool(2)
-		offset = pool.data.tell()
-		self.sized_str_entry = self.create_ss_entry(self.file_entry)
-		self.sized_str_entry.pointers[0].pool = pool
-		self.sized_str_entry.pointers[0].data_offset = offset
-
-		# read all in a dict from the xml
-		xmldata = ET.ElementTree(ET.fromstring(ss))
-		movie = xmldata.getroot()
-
-		self.uinamelist = [data.text for data in xmldata.findall('.//Control')]
-		self.uitriggerlist = [data.text for data in xmldata.findall('.//UITrigger')]
-		self.uiInterfacelist = [data.text for data in xmldata.findall('.//Interface')]
-		self.Count1List = [int(data.text) for data in xmldata.findall('.//List1')]
-		self.Count2List = [int(data.text) for data in xmldata.findall('.//List2')]
-
-		# writting the data in several chunks because of readability 
-		pool.data.write(struct.pack("<32sI2H", b'', int(movie.attrib['flags1']), int(movie.attrib['flags2']),
-									int(movie.attrib['flags3'])))
-		pool.data.write(struct.pack("<3f", float(movie.attrib['float1']), float(movie.attrib['float2']),
-									float(movie.attrib['float3'])))
-		pool.data.write(struct.pack("<4B", 0, len(self.uitriggerlist), 0, len(self.uinamelist)))
-		pool.data.write(struct.pack("<4B", 0, 0, len(self.Count1List), len(self.Count2List)))
-		pool.data.write(struct.pack("<4B", len(self.uiInterfacelist), 0, 0, 0))
-		pool.data.write(struct.pack("<80s", b''))
-
-		# write name and add ptr
-		nameptr = pool.data.tell()
-		pool.data.write(f"{movie.attrib['MovieName']}\00".encode('utf-8'))
-		new_frag = self.create_fragment()
-		new_frag.pointers[0].pool = pool
-		new_frag.pointers[0].data_offset = offset + 0x00
-		new_frag.pointers[1].pool = pool
-		new_frag.pointers[1].data_offset = nameptr
-
-		# write pkgname and add ptr
-		nameptr = pool.data.tell()
-		pool.data.write(f"{movie.attrib['PkgName']}\00".encode('utf-8'))
-		new_frag = self.create_fragment()
-		new_frag.pointers[0].pool = pool
-		new_frag.pointers[0].data_offset = offset + 0x08
-		new_frag.pointers[1].pool = pool
-		new_frag.pointers[1].data_offset = nameptr
-
-		# write CategoryName and add ptr
-		nameptr = pool.data.tell()
-		pool.data.write(f"{movie.attrib['CategoryName']}\00".encode('utf-8'))
-		new_frag = self.create_fragment()
-		new_frag.pointers[0].pool = pool
-		new_frag.pointers[0].data_offset = offset + 0x10
-		new_frag.pointers[1].pool = pool
-		new_frag.pointers[1].data_offset = nameptr
-
-		# write TypeName and add ptr
-		nameptr = pool.data.tell()
-		pool.data.write(f"{movie.attrib['TypeName']}\00".encode('utf-8'))
-		new_frag = self.create_fragment()
-		new_frag.pointers[0].pool = pool
-		new_frag.pointers[0].data_offset = offset + 0x18
-		new_frag.pointers[1].pool = pool
-		new_frag.pointers[1].data_offset = nameptr
-
-		# write triggers at offset+0x48
-		if len(self.uitriggerlist):
-
-			# offset where first string starts
-			doffset = pool.data.tell()
-
-			# pack data now.. we are not doing rstrip to the lines.. worth considering to remove extra spaces
-			pool.data.write("\00".join(self.uitriggerlist).encode('utf-8'))
-			pool.data.write(b"\00")
-
-			# new offset for list pointers
-			poffset = pool.data.tell()
-
-			# point the list frag to the end of the data now.
-			new_frag0 = self.create_fragment()
-			new_frag0.pointers[0].pool = pool
-			new_frag0.pointers[0].data_offset = offset + 0x48
-			new_frag0.pointers[1].pool = pool
-			new_frag0.pointers[1].data_offset = poffset
-
-			# for each line, add the frag ptr space and create the frag ptr
-			for x in self.uitriggerlist:
-				pool.data.write(struct.pack("<8s", b''))
-				strfrag = self.create_fragment()
-				strfrag.pointers[0].pool = pool
-				strfrag.pointers[0].data_offset = poffset
-				strfrag.pointers[1].pool = pool
-				strfrag.pointers[1].data_offset = doffset
-
-				poffset += 8
-				doffset += len(x) + 1  # skip string lenght
-
-		# write Controls at offset+0x58
-		if self.uinamelist:
-
-			# offset where first string starts
-			doffset = pool.data.tell()
-
-			# pack data now.. we are not doing rstrip to the lines.. worth considering to remove extra spaces
-			pool.data.write("\00".join(self.uinamelist).encode('utf-8'))
-			pool.data.write(b"\00")
-
-			# new offset for list pointers
-			poffset = pool.data.tell()
-
-			# point the list frag to the end of the data now.
-			new_frag0 = self.create_fragment()
-			new_frag0.pointers[0].pool = pool
-			new_frag0.pointers[0].data_offset = offset + 0x58
-			new_frag0.pointers[1].pool = pool
-			new_frag0.pointers[1].data_offset = poffset
-
-			# for each line, add the frag ptr space and create the frag ptr
-			for x in self.uinamelist:
-				pool.data.write(struct.pack("<8s", b''))
-				strfrag = self.create_fragment()
-				strfrag.pointers[0].pool = pool
-				strfrag.pointers[0].data_offset = poffset
-				strfrag.pointers[1].pool = pool
-				strfrag.pointers[1].data_offset = doffset
-
-				poffset += 8
-				doffset += len(x) + 1  # skip string lenght
-
-		# write List1 at offset+0x70
-		if len(self.Count1List):
-			# offset where first string starts
-			doffset = pool.data.tell()
-
-			# pack data now.. 
-			pool.data.write(struct.pack(f"<{len(self.Count1List)}I", *self.Count1List))
-			# add some extra 00
-			pool.data.write(struct.pack("<Q", 0))
-
-			# point the list frag to the end of the data now.
-			new_frag0 = self.create_fragment()
-			new_frag0.pointers[0].pool = pool
-			new_frag0.pointers[0].data_offset = offset + 0x70
-			new_frag0.pointers[1].pool = pool
-			new_frag0.pointers[1].data_offset = doffset
-
-		# write List2 at offset+0x78
-		if len(self.Count2List):
-			# offset where first string starts
-			doffset = pool.data.tell()
-
-			# pack data now.. 
-			pool.data.write(struct.pack(f"<{len(self.Count2List)}I", *self.Count2List))
-			# add some extra 00
-			pool.data.write(struct.pack("<Q", 0))
-
-			# point the list frag to the end of the data now.
-			new_frag0 = self.create_fragment()
-			new_frag0.pointers[0].pool = pool
-			new_frag0.pointers[0].data_offset = offset + 0x78
-			new_frag0.pointers[1].pool = pool
-			new_frag0.pointers[1].data_offset = doffset
-
-		# write interfaces at offset+0x80
-		if len(self.uiInterfacelist):
-
-			# offset where first string starts
-			doffset = pool.data.tell()
-
-			# pack data now.. we are not doing rstrip to the lines.. worth considering to remove extra spaces
-			pool.data.write("\00".join(self.uiInterfacelist).encode('utf-8'))
-			pool.data.write(b"\00")
-			# new offset for list pointers
-			poffset = pool.data.tell()
-
-			# point the list frag to the end of the data now.
-			new_frag0 = self.create_fragment()
-			new_frag0.pointers[0].pool = pool
-			new_frag0.pointers[0].data_offset = offset + 0x80
-			new_frag0.pointers[1].pool = pool
-			new_frag0.pointers[1].data_offset = poffset
-
-			# for each line, add the frag ptr space and create the frag ptr
-			for x in self.uiInterfacelist:
-				pool.data.write(struct.pack("<8s", b''))
-				strfrag = self.create_fragment()
-				strfrag.pointers[0].pool = pool
-				strfrag.pointers[0].data_offset = poffset
-				strfrag.pointers[1].pool = pool
-				strfrag.pointers[1].data_offset = doffset
-
-				poffset += 8
-				doffset += len(x) + 1  # skip string lenght
-
 	def collect(self):
 		self.assign_ss_entry()
 		logging.info(f"Collecting {self.sized_str_entry.name}")
@@ -352,18 +138,6 @@ class UIMovieDefinitionLoader(BaseFile):
 		unpackstr = "<32sI2H3f12B80s"
 		_, self.flags1, self.flags2, self.flags3, self.float1, self.float2, self.float3, counta, count4, countc, ctrlcount, assetpkgcount, countf, count1, count2, count3, countj, _, _, _ = \
 			struct.unpack(unpackstr, self.sized_str_entry.pointers[0].read_from_pool(0x90))
-
-
-		print(f"counta {counta}")
-		print(f"count4 {count4}")
-		print(f"countc {countc}")
-		print(f"ctrlcount {ctrlcount}")
-		print(f"assetpkgcount {assetpkgcount}")
-		print(f"countf {countf}")
-		print(f"count1 {count1}")
-		print(f"count2 {count2}")
-		print(f"count3 {count3}")
-		print(f"countj {countj}")
 
 		# get name
 		tmpfragment = self.ovs.frags_from_pointer(self.sized_str_entry.pointers[0], 1)[0]
