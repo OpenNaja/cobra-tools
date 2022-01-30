628cfa6dcf880fc254988e5be5520960c35c4c22
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-01-30 11:31:44 +0100

    Add inject warning for different png tiles

--- ovl_util/imarray.py
+++ ovl_util/imarray.py
@@ -48,15 +48,14 @@ def wrapper(png_file_path, header_7, ovl):
 	array_size = max(1, array_size)
 	if array_size > 1:
 		must_split = True
-	print("split_components", split_components)
-	print("must_split", must_split)
-	print("split_rg_b_a", split_rg_b_a)
-	print("must_flip_gb", must_flip_gb)
-	print("Splitting PNG array")
-	print(f"h {h}, w {w}, array_size {array_size}")
+	logging.debug(f"split_components {split_components}")
+	logging.debug(f"must_split {must_split}")
+	logging.debug(f"split_rg_b_a {split_rg_b_a}")
+	logging.debug(f"must_flip_gb {must_flip_gb}")
+	logging.debug("Splitting PNG array")
+	logging.debug(f"h {h}, w {w}, array_size {array_size}")
 	if must_split or must_flip_gb or split_components or split_rg_b_a:
 		im = imageio.imread(png_file_path)
-		# print(im.shape)
 		# (4096, 1024, 4)
 		h, w, d = im.shape
 		h //= array_size
@@ -128,6 +127,13 @@ def split_name_suffix(in_name):
 	return in_name, None
 
 
+def check_same_dimensions(dimensions, files):
+	"""Make sure that all array tiles have the same size"""
+	if not all(x == dimensions[0] for x in dimensions):
+		t_str = "\n".join([f"{im} [{h} x {w}]" for (h, w), im in zip(dimensions, files)])
+		raise AttributeError(f"Array tiles have different dimensions:\n{t_str}")
+
+
 def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 	"""This handles PNG modifications (arrays or flipped channels) and ensures the costly IO is only done once"""
 
@@ -157,10 +163,10 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 			return
 		out_file_paths.add(out_file_path)
 
-	print("must_join", must_join)
-	print("join_components", join_components)
-	print("join_rg_b_a", join_rg_b_a)
-	print("must_flip_gb", must_flip_gb)
+	logging.debug(f"must_join {must_join}")
+	logging.debug(f"join_components {join_components}")
+	logging.debug(f"join_rg_b_a {join_rg_b_a}")
+	logging.debug(f"must_flip_gb {must_flip_gb}")
 
 	# non-tiled files that need fixes - normal maps
 	if not must_join and not join_components:
@@ -172,26 +178,28 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 		array_textures = [file for file in os.listdir(in_dir) if is_array_tile(file, in_name_bare)]
 		# read all images into arrays
 		ims = [imageio.imread(os.path.join(in_dir, file)) for file in array_textures]
-		print("Array tile names:")
-		print(array_textures)
+		logging.debug(f"Array tile names: {array_textures}")
 		# load them all, then build im array from scratch
 		array_size = len(array_textures)
 		# check that all textures have the right dimensions
+		dimensions = []
 		for im, file in zip(ims, array_textures):
-			in_shape = ims[0].shape
 			# check for depth dimension
-			has_d = len(in_shape) == 3
+			has_d = len(im.shape) == 3
 			# RGBA or RGBA image
 			if has_d:
-				h, w, d = in_shape
+				h, w, d = im.shape
 				if d != 4:
 					# rgba files, obvious need to have RGB components, so don't complain
 					if not join_rg_b_a:
 						raise AttributeError(f"{file} does not have all 4 channels (RGBA) that are expected, it has {d}")
+				dimensions.append((h, w))
 			# no 3rd dimension, ie. single channel greyscale image
 			else:
-				h, w = in_shape
+				h, w = im.shape
 				d = 1
+				dimensions.append((h, w))
+		check_same_dimensions(dimensions, array_textures)
 		if join_components:
 			d = 4
 			array_size //= d
@@ -200,7 +208,7 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 			# since we have 2 components per tile
 			array_size //= 3
 
-		print("array_size", array_size)
+		logging.debug(f"array_size {array_size}")
 		if array_size == 0:
 			raise FileNotFoundError(
 				f"Only {len(array_textures)} array texture(s) were found in {in_dir}, resulting in an incomplete array. "
@@ -208,7 +216,7 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 		out_shape = (h * array_size, w, d)
 		im = np.zeros(out_shape, dtype=ims[0].dtype)
 		if join_components:
-			print("Rebuilding array texture from components")
+			logging.debug("Rebuilding array texture from components")
 			layer_i = 0
 			for hi in range(array_size):
 				for di in range(d):
@@ -216,12 +224,12 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 					if len(tile_shape) == 2:
 						im[hi * h:(hi + 1) * h, :, di] = ims[layer_i]
 					elif len(tile_shape) == 3:
-						print(
+						logging.warning(
 							f"Tile {array_textures[layer_i]} is not the expected single-channel float format, using first channel.")
 						im[hi * h:(hi + 1) * h, :, di] = ims[layer_i][:, :, 0]
 					layer_i += 1
 		elif join_rg_b_a:
-			print("Rebuilding array texture from RG + B + A")
+			logging.debug("Rebuilding array texture from RG + B + A")
 			layer_i = 0
 			for hi in range(array_size):
 				# RG
@@ -238,7 +246,7 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 				im[hi * h:(hi + 1) * h, :, 3] = get_single_channel(ims[layer_i], array_textures[layer_i])
 				layer_i += 1
 		else:
-			print("Rebuilding array texture from RGBA tiles")
+			logging.debug("Rebuilding array texture from RGBA tiles")
 			for layer_i in range(array_size):
 				im[layer_i * h:(layer_i + 1) * h, :, :] = ims[layer_i]
 
@@ -247,7 +255,7 @@ def inject_wrapper(png_file_path, out_file_paths, tmp_dir):
 		flip_gb(im)
 
 	# this is shared for all that have to be read
-	print("Writing png output")
+	logging.debug("Writing png output")
 	imageio.imwrite(out_file_path, im, compress_level=2)
 	return out_file_path
 
@@ -257,6 +265,5 @@ def get_single_channel(im, name):
 	if len(tile_shape) == 2:
 		return im
 	elif len(tile_shape) == 3:
-		print(
-			f"Tile {name} is not the expected single-channel float format, using first channel.")
+		logging.warning(f"Tile {name} is not the expected single-channel float format, using first channel.")
 		return im[:, :, 0]
