2a516b612f02d7514fbfdeeeac7842c549e39c5d
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-03-19 18:54:12 +0100

    Cleanup MemStruct

--- source/formats/ovl_base/compound/MemStruct.py
+++ source/formats/ovl_base/compound/MemStruct.py
@@ -20,35 +20,32 @@ class MemStruct:
 	def read_ptrs(self, ovs, ref_ptr, io_start=None):
 		if not io_start:
 			io_start = self.io_start
-		print("read_ptrs")
+		# print("read_ptrs")
 		# get all pointers in this struct
 		ptrs = self.get_ptrs()
 		for ptr in ptrs:
 			self.handle_ptr(ptr, ovs, ref_ptr, io_start)
 		arrays = self.get_arrays()
 		for array in arrays:
-			print("array")
+			# print("array")
 			for member in array:
 				if isinstance(member, MemStruct):
-					print("member is a memstruct")
+					# print("member is a memstruct")
 					member.read_ptrs(ovs, ref_ptr, array.io_start)
 
 	def handle_ptr(self, ptr, ovs, ref_ptr, io_start):
 		rel_offset = ptr.io_start-io_start
-		print(f"handle_ptr dtype: {ptr.template.__name__} relative: {rel_offset} count: {ptr.arg}")
+		# print(f"handle_ptr dtype: {ptr.template.__name__} relative: {rel_offset} count: {ptr.arg}")
 		# get a fragment that is relative to pointer + offset
 		f = ovs.frag_at_pointer(ref_ptr, offset=rel_offset)
 		# ptr may be a nullptr, so ignore
 		if not f:
-			print("is a nullptr")
+			# print("is a nullptr")
 			return
 		f_ptr = f.pointers[1]
 		ptr.data = ptr.template.from_stream(f_ptr.stream, ptr.context, ptr.arg)
-		if isinstance(ptr.data, Array):
-			print("is an array")
-			ptr.data.ref_ptr = f_ptr
 		if isinstance(ptr.data, MemStruct):
-			print("is a memstruct")
+			# print("is a memstruct")
 			ptr.data.read_ptrs(ovs, f_ptr)
 
 	def get_info_str(self):
