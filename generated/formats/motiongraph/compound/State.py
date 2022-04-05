97cdab1930c470d62d455c1cebf1e9d34e7bec2a
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-04-05 19:59:37 +0200

    Motiongraph

--- generated/formats/motiongraph/compound/State.py
+++ generated/formats/motiongraph/compound/State.py
@@ -1,6 +1,7 @@
 from source.formats.base.basic import fmt_member
 import generated.formats.base.basic
 import generated.formats.motiongraph.compound.PtrList
+import generated.formats.motiongraph.compound.TransStructStopList
 from generated.formats.ovl_base.compound.MemStruct import MemStruct
 from generated.formats.ovl_base.compound.Pointer import Pointer
 
@@ -20,20 +21,20 @@ class State(MemStruct):
 		self.io_size = 0
 		self.io_start = 0
 		self.unk = 0
-		self.count_1 = 0
+		self.activities_count = 0
 		self.count_2 = 0
-		self.array_1 = Pointer(self.context, self.count_1, generated.formats.motiongraph.compound.PtrList.PtrList)
-		self.array_2 = Pointer(self.context, self.count_2, generated.formats.motiongraph.compound.PtrList.PtrList)
+		self.activities = Pointer(self.context, self.activities_count, generated.formats.motiongraph.compound.PtrList.PtrList)
+		self.array_2 = Pointer(self.context, self.count_2, generated.formats.motiongraph.compound.TransStructStopList.TransStructStopList)
 		self.id = Pointer(self.context, 0, generated.formats.base.basic.ZString)
 		if set_default:
 			self.set_defaults()
 
 	def set_defaults(self):
 		self.unk = 0
-		self.count_1 = 0
+		self.activities_count = 0
 		self.count_2 = 0
-		self.array_1 = Pointer(self.context, self.count_1, generated.formats.motiongraph.compound.PtrList.PtrList)
-		self.array_2 = Pointer(self.context, self.count_2, generated.formats.motiongraph.compound.PtrList.PtrList)
+		self.activities = Pointer(self.context, self.activities_count, generated.formats.motiongraph.compound.PtrList.PtrList)
+		self.array_2 = Pointer(self.context, self.count_2, generated.formats.motiongraph.compound.TransStructStopList.TransStructStopList)
 		self.id = Pointer(self.context, 0, generated.formats.base.basic.ZString)
 
 	def read(self, stream):
@@ -50,12 +51,12 @@ class State(MemStruct):
 	def read_fields(cls, stream, instance):
 		super().read_fields(stream, instance)
 		instance.unk = stream.read_uint()
-		instance.count_1 = stream.read_uint()
-		instance.array_1 = Pointer.from_stream(stream, instance.context, instance.count_1, generated.formats.motiongraph.compound.PtrList.PtrList)
+		instance.activities_count = stream.read_uint()
+		instance.activities = Pointer.from_stream(stream, instance.context, instance.activities_count, generated.formats.motiongraph.compound.PtrList.PtrList)
 		instance.count_2 = stream.read_uint64()
-		instance.array_2 = Pointer.from_stream(stream, instance.context, instance.count_2, generated.formats.motiongraph.compound.PtrList.PtrList)
+		instance.array_2 = Pointer.from_stream(stream, instance.context, instance.count_2, generated.formats.motiongraph.compound.TransStructStopList.TransStructStopList)
 		instance.id = Pointer.from_stream(stream, instance.context, 0, generated.formats.base.basic.ZString)
-		instance.array_1.arg = instance.count_1
+		instance.activities.arg = instance.activities_count
 		instance.array_2.arg = instance.count_2
 		instance.id.arg = 0
 
@@ -63,8 +64,8 @@ class State(MemStruct):
 	def write_fields(cls, stream, instance):
 		super().write_fields(stream, instance)
 		stream.write_uint(instance.unk)
-		stream.write_uint(instance.count_1)
-		Pointer.to_stream(stream, instance.array_1)
+		stream.write_uint(instance.activities_count)
+		Pointer.to_stream(stream, instance.activities)
 		stream.write_uint64(instance.count_2)
 		Pointer.to_stream(stream, instance.array_2)
 		Pointer.to_stream(stream, instance.id)
@@ -91,8 +92,8 @@ class State(MemStruct):
 		s = ''
 		s += super().get_fields_str()
 		s += f'\n	* unk = {fmt_member(self.unk, indent+1)}'
-		s += f'\n	* count_1 = {fmt_member(self.count_1, indent+1)}'
-		s += f'\n	* array_1 = {fmt_member(self.array_1, indent+1)}'
+		s += f'\n	* activities_count = {fmt_member(self.activities_count, indent+1)}'
+		s += f'\n	* activities = {fmt_member(self.activities, indent+1)}'
 		s += f'\n	* count_2 = {fmt_member(self.count_2, indent+1)}'
 		s += f'\n	* array_2 = {fmt_member(self.array_2, indent+1)}'
 		s += f'\n	* id = {fmt_member(self.id, indent+1)}'
