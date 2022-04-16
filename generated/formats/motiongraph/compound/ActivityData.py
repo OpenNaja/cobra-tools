b8dc5f89908d7075c4bfca4516820c7ed52414ef
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-04-16 16:10:30 +0200

    Refactor

---
+++ generated/formats/motiongraph/compound/ActivityData.py
@@ -0,0 +1,77 @@
+from source.formats.base.basic import fmt_member
+from generated.formats.motiongraph.compound.AnimationActivityData import AnimationActivityData
+from generated.formats.ovl_base.compound.MemStruct import MemStruct
+
+
+class ActivityData(MemStruct):
+
+	"""
+	# todo - investigate if this is a viable alternative, change codegen if needed to support string comparison
+	"""
+
+	def __init__(self, context, arg=0, template=None, set_default=True):
+		self.name = ''
+		super().__init__(context, arg, template, set_default)
+		self.arg = arg
+		self.template = template
+		self.io_size = 0
+		self.io_start = 0
+		self.data = AnimationActivityData(self.context, 0, None)
+		if set_default:
+			self.set_defaults()
+
+	def set_defaults(self):
+		if self.arg == self.animation_activity:
+			self.data = AnimationActivityData(self.context, 0, None)
+
+	def read(self, stream):
+		self.io_start = stream.tell()
+		self.read_fields(stream, self)
+		self.io_size = stream.tell() - self.io_start
+
+	def write(self, stream):
+		self.io_start = stream.tell()
+		self.write_fields(stream, self)
+		self.io_size = stream.tell() - self.io_start
+
+	@classmethod
+	def read_fields(cls, stream, instance):
+		super().read_fields(stream, instance)
+		if instance.arg == instance.animation_activity:
+			instance.data = AnimationActivityData.from_stream(stream, instance.context, 0, None)
+
+	@classmethod
+	def write_fields(cls, stream, instance):
+		super().write_fields(stream, instance)
+		if instance.arg == instance.animation_activity:
+			AnimationActivityData.to_stream(stream, instance.data)
+
+	@classmethod
+	def from_stream(cls, stream, context, arg=0, template=None):
+		instance = cls(context, arg, template, set_default=False)
+		instance.io_start = stream.tell()
+		cls.read_fields(stream, instance)
+		instance.io_size = stream.tell() - instance.io_start
+		return instance
+
+	@classmethod
+	def to_stream(cls, stream, instance):
+		instance.io_start = stream.tell()
+		cls.write_fields(stream, instance)
+		instance.io_size = stream.tell() - instance.io_start
+		return instance
+
+	def get_info_str(self, indent=0):
+		return f'ActivityData [Size: {self.io_size}, Address: {self.io_start}] {self.name}'
+
+	def get_fields_str(self, indent=0):
+		s = ''
+		s += super().get_fields_str()
+		s += f'\n	* data = {fmt_member(self.data, indent+1)}'
+		return s
+
+	def __repr__(self, indent=0):
+		s = self.get_info_str(indent)
+		s += self.get_fields_str(indent)
+		s += '\n'
+		return s
