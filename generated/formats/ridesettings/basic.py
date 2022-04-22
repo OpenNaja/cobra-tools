5bba4aeb640e8d282ed2c38ee73049bcb0668127
Florian CUNY <poslovitch@bentobox.world>
2022-04-22 23:22:30 +0200

    Added basic support for .ridesettings files

---
+++ generated/formats/ridesettings/basic.py
@@ -0,0 +1,19 @@
+
+
+from generated.formats.ovl_base.basic import Byte, Ubyte, Uint64, Int64, Uint, Ushort, Int, Short, Char, Float, ZString, Bool, ZStringObfuscated
+
+basic_map = {
+			'Byte': Byte,
+			'Ubyte': Ubyte,
+			'Uint64': Uint64,
+			'Int64': Int64,
+			'Uint': Uint,
+			'Ushort': Ushort,
+			'Int': Int,
+			'Short': Short,
+			'Char': Char,
+			'Float': Float,
+			'ZString': ZString,
+			'Bool': Bool,
+			'ZStringObfuscated': ZStringObfuscated,
+}
