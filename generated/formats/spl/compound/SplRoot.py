80d9b5dd10f5ccc3e321eb9cdabfb4ecd70242fd
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-04-26 18:47:28 +0200

    Port Spl

---
+++ generated/formats/spl/compound/SplRoot.py
@@ -0,0 +1,96 @@
+from source.formats.base.basic import fmt_member
+from generated.formats.ovl_base.compound.MemStruct import MemStruct
+from generated.formats.ovl_base.compound.Pointer import Pointer
+
+
+class SplRoot(MemStruct):
+
+	"""
+	JWE2: 16 bytes
+	very weird, related to count
+	"""
+
+	def __init__(self, context, arg=0, template=None, set_default=True):
+		self.name = ''
+		super().__init__(context, arg, template, set_default)
+		self.arg = arg
+		self.template = template
+		self.io_size = 0
+		self.io_start = 0
+		self.count = 0
+		self.sixteen = 0
+		self.one = 0
+		self.length = 0.0
+		self.spline_data = Pointer(self.context, self.count, None)
+		if set_default:
+			self.set_defaults()
+
+	def set_defaults(self):
+		self.count = 0
+		self.sixteen = 0
+		self.one = 0
+		self.length = 0.0
+		self.spline_data = Pointer(self.context, self.count, None)
+
+	def read(self, stream):
+		self.io_start = stream.tell()
+		self.read_fields(stream, self)
+		self.io_size = stream.tell() - self.io_start
+
+	def write(self, stream):
+		self.io_start = stream.tell()
+		self.write_fields(stream, self)
+		self.io_size = stream.tell() - self.io_start
+
+	@classmethod
+	def read_fields(cls, stream, instance):
+		super().read_fields(stream, instance)
+		instance.spline_data = Pointer.from_stream(stream, instance.context, instance.count, None)
+		instance.count = stream.read_ushort()
+		instance.sixteen = stream.read_ubyte()
+		instance.one = stream.read_ubyte()
+		instance.length = stream.read_float()
+		instance.spline_data.arg = instance.count
+
+	@classmethod
+	def write_fields(cls, stream, instance):
+		super().write_fields(stream, instance)
+		Pointer.to_stream(stream, instance.spline_data)
+		stream.write_ushort(instance.count)
+		stream.write_ubyte(instance.sixteen)
+		stream.write_ubyte(instance.one)
+		stream.write_float(instance.length)
+
+	@classmethod
+	def from_stream(cls, stream, context, arg=0, template=None):
+		instance = cls(context, arg, template, set_default=False)
+		instance.io_start = stream.tell()
+		cls.read_fields(stream, instance)
+		instance.io_size = stream.tell() - instance.io_start
+		return instance
+
+	@classmethod
+	def to_stream(cls, stream, instance):
+		instance.io_start = stream.tell()
+		cls.write_fields(stream, instance)
+		instance.io_size = stream.tell() - instance.io_start
+		return instance
+
+	def get_info_str(self, indent=0):
+		return f'SplRoot [Size: {self.io_size}, Address: {self.io_start}] {self.name}'
+
+	def get_fields_str(self, indent=0):
+		s = ''
+		s += super().get_fields_str()
+		s += f'\n	* spline_data = {fmt_member(self.spline_data, indent+1)}'
+		s += f'\n	* count = {fmt_member(self.count, indent+1)}'
+		s += f'\n	* sixteen = {fmt_member(self.sixteen, indent+1)}'
+		s += f'\n	* one = {fmt_member(self.one, indent+1)}'
+		s += f'\n	* length = {fmt_member(self.length, indent+1)}'
+		return s
+
+	def __repr__(self, indent=0):
+		s = self.get_info_str(indent)
+		s += self.get_fields_str(indent)
+		s += '\n'
+		return s
--- modules/formats/SPL.py
+++ modules/formats/SPL.py
@@ -1,50 +1,8 @@
-import logging
-import struct
-from modules.formats.shared import get_padding
-from modules.formats.BaseFormat import BaseFile
-from modules.helpers import zstr
+from generated.formats.spl.compound.SplRoot import SplRoot
+from modules.formats.BaseFormat import MemStructLoader
 
 
-class SplineLoader(BaseFile):
+class SplineLoader(MemStructLoader):
 	extension = ".spl"
+	target_class = SplRoot
 
-	def create(self):
-		if True:
-			pass
-		f_0, f_1 = self._get_data(self.file_entry.path)
-		self.sized_str_entry = self.create_ss_entry(self.file_entry)
-		f = self.create_fragments(self.sized_str_entry, 1)[0]
-		self.write_to_pool(f.pointers[1], 2, f_1)
-		self.write_to_pool(self.sized_str_entry.pointers[0], 4, b"")
-		self.write_to_pool(f.pointers[0], 4, f_0)
-
-	def collect(self):
-		if True:
-			pass
-		self.assign_ss_entry()
-		self.assign_fixed_frags(1)
-
-	def load(self, file_path):
-		if True:
-			pass
-		f_0, f_1 = self._get_data(file_path)
-		self.sized_str_entry.fragments[0].pointers[1].update_data(f_1, update_copies=True)
-
-	def extract(self, out_dir, show_temp_files, progress_callback):
-		if True:
-			pass
-		name = self.sized_str_entry.name
-		logging.info(f"Writing {name}")
-		f_0 = self.sized_str_entry.fragments[0]
-		out_path = out_dir(name)
-		with open(out_path, 'wb') as outfile:
-			f_0.pointers[1].strip_zstring_padding()
-			outfile.write(f_0.pointers[1].data[:-1])
-		return out_path,
-
-	def _get_data(self, file_path):
-		"""Loads and returns the data for a CURVE"""
-		# copy content, pad to 64b, then assign 1 fragment and 1 empty sized str.
-		f_0 = struct.pack('16s', b'')  # fragment pointer 0 data
-		f_1 = zstr(self.get_content(file_path))  # fragment pointer 1 data
-		return f_0, f_1 + get_padding(len(f_1), alignment=64)
