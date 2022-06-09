0d8ae51cc8a3612253f7bde3afb89c21d247fe65
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-06-09 21:39:52 +0200

    Add FCT create

--- modules/formats/FCT.py
+++ modules/formats/FCT.py
@@ -1,3 +1,6 @@
+import io
+import os
+
 from generated.formats.fct.compound.FctRoot import FctRoot
 from modules.formats.BaseFormat import MemStructLoader
 
@@ -5,6 +8,9 @@ from modules.formats.BaseFormat import MemStructLoader
 class FctLoader(MemStructLoader):
 	extension = ".fct"
 	target_class = FctRoot
+
+	def get_font_name(self, i, ext):
+		return f"{self.file_entry.basename}_font_{i}{ext}"
 
 	def extract(self, out_dir, show_temp_files, progress_callback):
 		out_files = super().extract(out_dir, show_temp_files, progress_callback)
@@ -19,71 +25,34 @@ class FctLoader(MemStructLoader):
 					ext = ".otf"
 				else:
 					ext = ".ttf"
-				path = f"{out_dir(self.file_entry.basename)}_font_{i}{ext}"
+				path = out_dir(self.get_font_name(i, ext))
 				paths.append(path)
 				with open(path, 'wb') as outfile:
 					outfile.write(buff[offset:offset + font.data_size])
 			offset += font.data_size
 		return paths
 
-	# def load(self, file_path):
-	# 	# todo - fixme
-	# 	# read fct
-	# 	# inject fct buffers
-	# 	# update sized string
-	# 	ss_len = len(self.root_entry.struct_ptr.data) / 4
-	# 	ss_data = list(struct.unpack("<4f{}I".format(int(ss_len - 4)), self.root_entry.struct_ptr.data))
-	# 	pad_size = ss_data[8]
-	# 	data_sizes = (ss_data[10], ss_data[12], ss_data[14], ss_data[16])
-	# 	old_buffer_bytes = self.data_entry.buffer_datas[0]
-	# 	print("old", len(old_buffer_bytes))
-	# 	pad_bytes = old_buffer_bytes[0:pad_size]
-	# 	d0 = old_buffer_bytes[pad_size:data_sizes[0] + pad_size]
-	# 	d1 = old_buffer_bytes[data_sizes[0] + pad_size:data_sizes[0] + pad_size + data_sizes[1]]
-	# 	d2 = old_buffer_bytes[
-	# 		 data_sizes[0] + pad_size + data_sizes[1]:data_sizes[0] + pad_size + data_sizes[1] + data_sizes[2]]
-	# 	d3 = old_buffer_bytes[data_sizes[0] + pad_size + data_sizes[1] + data_sizes[2]:]
-	# 	print("old2", len(pad_bytes + d0 + d1 + d2 + d3))
-	#
-	# 	# data_size = ss_data[10]
-	# 	name_ext, name, ext = split_path(file_path)
-	# 	ind = int(name[-1])
-	# 	print("updating index: ", ind)
-	#
-	# 	with open(file_path, "rb") as stream:
-	# 		# load the new buffer
-	# 		new_buffer_bytes = stream.read()
-	#
-	# 		buffer_bytes = pad_bytes  # update the correct root_entry entry size
-	# 		if ind == 0:
-	# 			ss_data[10] = len(new_buffer_bytes)
-	# 			buffer_bytes += new_buffer_bytes
-	# 			buffer_bytes += d1
-	# 			buffer_bytes += d2
-	# 			buffer_bytes += d3
-	# 		elif ind == 1:
-	# 			ss_data[12] = len(new_buffer_bytes)
-	# 			buffer_bytes += d0
-	# 			buffer_bytes += new_buffer_bytes
-	# 			buffer_bytes += d2
-	# 			buffer_bytes += d3
-	# 		elif ind == 2:
-	# 			ss_data[14] = len(new_buffer_bytes)
-	# 			buffer_bytes += d0
-	# 			buffer_bytes += d1
-	# 			buffer_bytes += new_buffer_bytes
-	# 			buffer_bytes += d3
-	# 		elif ind == 3:
-	# 			ss_data[16] = len(new_buffer_bytes)
-	# 			buffer_bytes += d0
-	# 			buffer_bytes += d1
-	# 			buffer_bytes += d2
-	# 			buffer_bytes += new_buffer_bytes
-	#
-	# 		print(len(buffer_bytes))
-	#
-	# 		# update the buffers
-	# 		self.data_entry.update_data((buffer_bytes,))
-	#
-	# 		data = struct.pack("<4f{}I".format(int(ss_len - 4)), *ss_data)
-	# 		self.write_data_to_pool(self.root_entry.struct_ptr, 2, data)
+	def create(self):
+		self.create_root_entry()
+		self.header = self.target_class.from_xml_file(self.file_entry.path, self.ovl.context)
+		file_dir = os.path.dirname(self.file_entry.path)
+		with io.BytesIO() as buff_stream:
+			buff_stream.write(b'\x00' * self.header.offset)
+			for i, font in enumerate(self.header.fonts):
+				# see if a matching font exists
+				for ext in (".otf", ".ttf"):
+					file_name = self.get_font_name(i, ext)
+					file_path = os.path.join(file_dir, file_name)
+					if os.path.isfile(file_path):
+						# load and store its data
+						with open(file_path, "rb") as f:
+							font_bytes = f.read()
+						buff_stream.write(font_bytes)
+						font.data_size = len(font_bytes)
+						break
+				else:
+					font.data_size = 0
+			# write data
+			self.create_data_entry((buff_stream.getvalue(),))
+			self.header.write_ptrs(self, self.root_ptr, self.file_entry.pool_type)
+
