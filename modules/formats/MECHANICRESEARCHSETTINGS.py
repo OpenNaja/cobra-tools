b4b910f325013dae2b877939edb7ba60ace89f9e
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-06-25 07:45:54 +0200

    Cleanup mechanicresearch mime by Jon

--- modules/formats/MECHANICRESEARCHSETTINGS.py
+++ modules/formats/MECHANICRESEARCHSETTINGS.py
@@ -1,5 +1,4 @@
 from generated.formats.mechanicresearch.compound.ResearchRoot import ResearchRoot
-#from generated.formats.mechanicresearch.compound.ResearchStartRoot import ResearchStartRoot
 from modules.formats.BaseFormat import MemStructLoader
 
 
@@ -7,18 +6,11 @@ class MechanicresearchsettingsLoader(MemStructLoader):
 	target_class = ResearchRoot
 	extension = ".mechanicresearchsettings"
 
-	#def collect(self):
-	#	super().collect()
-		# self.header.debug_ptrs()
-		# print(self.header)
-
 	def prep(self):
 		# avoid generating pointers for these
 		for research in self.header.levels.data:
 			if not research.next_research_count:
 				research.next_research.data = None
-			if not research.children_count:
-				research.children.data = None
 
 	def create(self):
 		self.create_root_entry()
