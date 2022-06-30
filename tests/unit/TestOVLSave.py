95782cd6b3a3a4db7b0f863a413c4884e80b5404
Jon <170077+hexabits@users.noreply.github.com>
2022-06-30 01:23:41 -0400

    Fix tests for 9db8f449

--- tests/unit/TestOVLSave.py
+++ tests/unit/TestOVLSave.py
@@ -19,7 +19,7 @@ class TestOVLSave(unittest.TestCase):
 		self.assertEqual(game, get_game(self.ovlfile)[0].value, "Should have the same game")
 
 	def test_ovl_save_pz(self):
-		game = "Planet Zoo 1.6+"
+		game = "Planet Zoo"
 		file = 'tests/tmp/pz.ovl'
 		set_game(self.ovlfile.context, game)
 		set_game(self.ovlfile, game)
