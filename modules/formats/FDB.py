d033b5377e4bfd27af18154fd8ca210f9b44cd23
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-07-01 06:49:40 +0200

    Cleanup fdb code

--- modules/formats/FDB.py
+++ modules/formats/FDB.py
@@ -24,10 +24,7 @@ class FdbLoader(BaseFile):
 
 	def extract(self, out_dir):
 		name = self.root_entry.name
-		try:
-			buff = self.data_entry.buffer_datas[1]
-		except:
-			raise AttributeError(f"Found no buffer data for {name}")
+		buff = self.data_entry.buffer_datas[1]
 		out_path = out_dir(name)
 		with open(out_path, 'wb') as outfile:
 			outfile.write(buff)
@@ -40,20 +37,24 @@ class FdbLoader(BaseFile):
 		root_entry = struct.pack("I28s", len(buffer_1), b'')
 		return root_entry, buffer_0, buffer_1
 
-	def open_command(self, f):
-		command_path = os.path.join(root_dir, "sql_commands", f+".sql")
+	@staticmethod
+	def open_command(f):
+		command_path = os.path.join(root_dir, "sql_commands", f"{f}.sql")
 		with open(command_path, "r") as file:
 			return file.read()
 
 	ScriptContext = namedtuple('ScriptContext', ['name', 'command', 'num_strings', 'find_strings'])
+
 	def context_is_valid(self, context: ScriptContext, name_tuples):
 		if not context or not name_tuples:
 			return False
 		# Require at least N tuples from the GUI, strings over context.num_strings are ignored
 		if len(name_tuples) < context.num_strings:
 			logging.warning(f"Required replacement strings missing for {self.file_entry.name}")
-			logging.info(f"Skipping {self.file_entry.name}. Script strings in need of replacement: {context.find_strings}")
-			logging.info(f"Rename Contents on this FDB needs {context.num_strings} strings in both Find/Replace separated by new lines.")
+			logging.info(
+				f"Skipping {self.file_entry.name}. Script strings in need of replacement: {context.find_strings}")
+			logging.info(
+				f"Rename Contents on this FDB needs {context.num_strings} strings in both Find/Replace separated by new lines.")
 			return False
 		return context.name and context.command and context.find_strings
 
@@ -68,27 +69,27 @@ class FdbLoader(BaseFile):
 		logging.error(f'SQL error: {not_found} not found in {self.file_entry.name}. Aborting SQL commands.')
 		return False
 
-	def rename_content(self, name_tuples):
-		# The current script context e.g. "animals" or "research"
-		context = None
+	def get_script_context(self):
 		# The SQL strings per script
 		script_strings = {
-			"animals" : [("ORIGINAL_SPECIES", "NEW_SPECIES"), ("ORIGINAL_PREFAB", "NEW_PREFAB")],
-			"education" : [("ORIGINAL_SPECIES", "NEW_SPECIES")],
-			"research" : [("ORIGINAL_SPECIES", "NEW_SPECIES")],
+			"animals": [("ORIGINAL_SPECIES", "NEW_SPECIES"), ("ORIGINAL_PREFAB", "NEW_PREFAB")],
+			"education": [("ORIGINAL_SPECIES", "NEW_SPECIES")],
+			"research": [("ORIGINAL_SPECIES", "NEW_SPECIES")],
 			"zoopedia": [("ORIGINAL_SPECIES", "NEW_SPECIES")]
 		}
-
 		if is_pz(self.ovl) or is_pz16(self.ovl):
-			for s in ("zoopedia", "research", "education", "animals"):
+			for s in script_strings.keys():
 				if s in self.file_entry.name:
 					# The SQL strings for the current context
 					find_strings = list(script_strings.get(s, [()]))
-					context = self.ScriptContext(s, self.open_command(f"pz_{s}"), len(find_strings), find_strings)
-					break
+					return self.ScriptContext(s, self.open_command(f"pz_{s}"), len(find_strings), find_strings)
+
+	def rename_content(self, name_tuples):
+		# The current script context e.g. "animals" or "research"
+		context = self.get_script_context()
 
 		if self.context_is_valid(context, name_tuples):
-			logging.info(f"Executing command '{s}' on {self.file_entry.name}")
+			logging.info(f"Executing command '{context.name}' on {self.file_entry.name}")
 			try:
 				temp_dir, out_dir_func = self.get_tmp_dir()
 				fdb_path = self.extract(out_dir_func)[0]
@@ -109,13 +110,16 @@ class FdbLoader(BaseFile):
 					try:
 						command_replaced = context.command
 						for i, find in enumerate(context.find_strings):
-							command_replaced = command_replaced.replace(find[0], name_tuples[i][0]).replace(find[1], name_tuples[i][1])
-						
+							command_replaced = command_replaced.replace(find[0], name_tuples[i][0]).replace(find[1],
+																											name_tuples[
+																												i][1])
+
 						# Calculate new research hash
 						# CALCULATED_HASH gets added to the original ResearchID in the SQL script
 						# Uses both Find and Replace strings for reduced chance of collisions
 						if context.name == "research":
-							command_replaced = command_replaced.replace("CALCULATED_HASH", str(djb2(name_tuples[0][0] + name_tuples[0][1])))
+							command_replaced = command_replaced.replace("CALCULATED_HASH", str(djb2(
+								name_tuples[0][0] + name_tuples[0][1])))
 
 						cur.executescript(command_replaced)
 						# Save (commit) the changes
@@ -131,5 +135,6 @@ class FdbLoader(BaseFile):
 			except BaseException as err:
 				traceback.print_exc()
 		elif context:
-			logging.error(f'SQL Script context "{context.name}" invalid on {self.file_entry.name}')
+			logging.error(f"SQL Script context '{context.name}' invalid on {self.file_entry.name}")
+
 
