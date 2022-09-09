2bb7bf405a0345855b80e7c5bb82944e17790f6b
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-09-09 12:56:40 +0200

    Re-enable motiongraph

--- modules/formats/MOTIONGRAPH.py
+++ modules/formats/MOTIONGRAPH.py
@@ -2,10 +2,10 @@ from generated.formats.motiongraph.compounds.MotiongraphHeader import Motiongrap
 from modules.formats.BaseFormat import MemStructLoader
 
 
-# class MotiongraphLoader(MemStructLoader):
-# 	target_class = MotiongraphHeader
-# 	extension = ".motiongraph"
-#
-# 	def collect(self):
-# 		if self.ovl.version >= 19:
-# 			super().collect()
+class MotiongraphLoader(MemStructLoader):
+	target_class = MotiongraphHeader
+	extension = ".motiongraph"
+
+	def collect(self):
+		if self.ovl.version >= 19:
+			super().collect()
