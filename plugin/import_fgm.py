7844fe21e77a381e17019929ff35454581865c93
ilodev <ilodev@users.noreply.github.com>
2022-09-16 03:52:19 -0700

    Update import_fgm.py
    To get rid of an error when importing fgm files in blender

--- plugin/import_fgm.py
+++ plugin/import_fgm.py
@@ -5,11 +5,14 @@ from plugin.modules_import.material import create_material
 
 
 def load(filepath):
-    created_materials = []
+    messages = set()
     in_dir, material_ext = os.path.split(filepath)
     material, ext = os.path.splitext(material_ext)
-    b_me = bpy.context.object.data
+    #created_materials = []
     # import_material(created_materials, in_dir, b_me, material)
-
     b_mat = create_material(in_dir, material)
+    b_me = bpy.context.object.data
     b_me.materials.append(b_mat)
+
+    messages.add(f"Finished fgm import")
+    return messages
