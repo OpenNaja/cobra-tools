0f6a29ac344ca307fd9a3eee4dc27bbce84599e9
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-10-11 22:16:20 +0200

    Fix signatures for to_stream

--- generated/formats/ms2/basic.py
+++ generated/formats/ms2/basic.py
@@ -22,18 +22,18 @@ class MainVersion(Uint):
 
 
 class OffsetString(Uint):
-	pass
-	# @classmethod
-	# def from_stream(cls, stream, context, arg=0, template=None):
-	# 	offset = super().from_stream(stream, context, arg, template)
-	# 	# arg must be ZStringBuffer
-	# 	return arg.get_str_at(offset)
-	#
-	# @staticmethod
-	# def to_stream(stream, instance):
-	# 	# arg = ZStringBuffer needs to be filled before writing
-	# 	# now we just take the index prepared by the string table
-	# 	raise NotImplementedError("OffsetString needs implementation for writing")
+
+	@classmethod
+	def from_stream(cls, stream, context, arg=0, template=None):
+		offset = super().from_stream(stream, context, arg, template)
+		# arg must be ZStringBuffer
+		return arg.get_str_at(offset)
+
+	@staticmethod
+	def to_stream(instance, stream, context, arg=0, template=None):
+		# arg = ZStringBuffer needs to be filled before writing
+		# now we just take the index prepared by the string table
+		raise NotImplementedError("OffsetString needs implementation for writing")
 
 
 from generated.formats.ovl_base.basic import Byte, Ubyte, Uint64, Int64, Uint, Ushort, Int, Short, Char, Float, Double, ZString, Bool, ZStringObfuscated
