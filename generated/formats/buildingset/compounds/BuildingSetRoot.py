f7842a851f561294e0a7d02c4a774c5410365c4f
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-10-12 21:58:16 +0200

    Fix BuildingSetRoot

--- generated/formats/buildingset/compounds/BuildingSetRoot.py
+++ generated/formats/buildingset/compounds/BuildingSetRoot.py
@@ -1,15 +1,10 @@
 from generated.formats.base.basic import Uint64
-from generated.formats.base.basic import ZString
+from generated.formats.ovl_base.basic import ZStringObfuscated
 from generated.formats.ovl_base.compounds.MemStruct import MemStruct
 from generated.formats.ovl_base.compounds.Pointer import Pointer
 
 
 class BuildingSetRoot(MemStruct):
-
-	"""
-	# needs to be transformed into readable: 4260428428::CvjmejohTfu
-	#                                        djbhash::BuildingSet
-	"""
 
 	__name__ = 'BuildingSetRoot'
 
@@ -20,12 +15,12 @@ class BuildingSetRoot(MemStruct):
 		self.set_count_or_type = 0
 		self.unk_1_found_as_0 = 0
 		self.unk_2_found_as_0 = 0
-		self.set_id_name = Pointer(self.context, 0, ZString)
+		self.set_id_name = Pointer(self.context, 0, ZStringObfuscated)
 		if set_default:
 			self.set_defaults()
 
 	_attribute_list = MemStruct._attribute_list + [
-		('set_id_name', Pointer, (0, ZString), (False, None), None),
+		('set_id_name', Pointer, (0, ZStringObfuscated), (False, None), None),
 		('set_count_or_type', Uint64, (0, None), (False, None), None),
 		('unk_1_found_as_0', Uint64, (0, None), (False, None), None),
 		('unk_2_found_as_0', Uint64, (0, None), (False, None), None),
@@ -34,7 +29,7 @@ class BuildingSetRoot(MemStruct):
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
-		yield 'set_id_name', Pointer, (0, ZString), (False, None)
+		yield 'set_id_name', Pointer, (0, ZStringObfuscated), (False, None)
 		yield 'set_count_or_type', Uint64, (0, None), (False, None)
 		yield 'unk_1_found_as_0', Uint64, (0, None), (False, None)
 		yield 'unk_2_found_as_0', Uint64, (0, None), (False, None)
