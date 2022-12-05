8b89d49cd61af31b36a51b336572713e3b85e5e2
HENDRIX-ZT2 <arnfinn@spholderer.de>
2022-12-05 16:29:10 +0100

    Improve MeshCollision

--- generated/formats/ms2/compounds/MeshCollisionBit.py
+++ generated/formats/ms2/compounds/MeshCollisionBit.py
@@ -15,7 +15,16 @@ class MeshCollisionBit(BaseStruct):
 		super().__init__(context, arg, template, set_default=False)
 
 		# ?
-		self.countd = Array(self.context, 0, None, (0,), Ushort)
+		self.a = Array(self.context, 0, None, (0,), Ushort)
+
+		# incrementing by 16 from 0, or if around 32769: incrementing by 1, mostly
+		self.b = Array(self.context, 0, None, (0,), Ushort)
+
+		# usually, but not always the first value
+		self.min_of_b = 0
+
+		# ?
+		self.c = 0
 
 		# always 2954754766?
 		self.consts = Array(self.context, 0, None, (0,), Uint)
@@ -23,12 +32,18 @@ class MeshCollisionBit(BaseStruct):
 			self.set_defaults()
 
 	_attribute_list = BaseStruct._attribute_list + [
-		('countd', Array, (0, None, (34,), Ushort), (False, None), None),
+		('a', Array, (0, None, (24,), Ushort), (False, None), None),
+		('b', Array, (0, None, (8,), Ushort), (False, None), None),
+		('min_of_b', Ushort, (0, None), (False, None), None),
+		('c', Ushort, (0, None), (False, None), None),
 		('consts', Array, (0, None, (3,), Uint), (False, None), None),
 		]
 
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
-		yield 'countd', Array, (0, None, (34,), Ushort), (False, None)
+		yield 'a', Array, (0, None, (24,), Ushort), (False, None)
+		yield 'b', Array, (0, None, (8,), Ushort), (False, None)
+		yield 'min_of_b', Ushort, (0, None), (False, None)
+		yield 'c', Ushort, (0, None), (False, None)
 		yield 'consts', Array, (0, None, (3,), Uint), (False, None)
