47d8f09530bfff6a44a8cfe957e2aedeedbceb8e
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-05-14 23:02:07 +0200

    Improve specdef

--- generated/formats/specdef/compounds/BooleanData.py
+++ generated/formats/specdef/compounds/BooleanData.py
@@ -14,8 +14,8 @@ class BooleanData(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.value = name_type_map['Ubyte'](self.context, 0, None)
-		self.default = name_type_map['Ubyte'](self.context, 0, None)
+		self.ivalue = name_type_map['Ubyte'](self.context, 0, None)
+		self.ioptional = name_type_map['Ubyte'](self.context, 0, None)
 		self.unused = Array(self.context, 0, None, (0,), name_type_map['Ubyte'])
 		if set_default:
 			self.set_defaults()
@@ -23,13 +23,13 @@ class BooleanData(MemStruct):
 	@classmethod
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
-		yield 'value', name_type_map['Ubyte'], (0, None), (False, None), (None, None)
-		yield 'default', name_type_map['Ubyte'], (0, None), (False, None), (None, None)
+		yield 'ivalue', name_type_map['Ubyte'], (0, None), (False, None), (None, None)
+		yield 'ioptional', name_type_map['Ubyte'], (0, None), (False, None), (None, None)
 		yield 'unused', Array, (0, None, (6,), name_type_map['Ubyte']), (False, None), (None, None)
 
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
-		yield 'value', name_type_map['Ubyte'], (0, None), (False, None)
-		yield 'default', name_type_map['Ubyte'], (0, None), (False, None)
+		yield 'ivalue', name_type_map['Ubyte'], (0, None), (False, None)
+		yield 'ioptional', name_type_map['Ubyte'], (0, None), (False, None)
 		yield 'unused', Array, (0, None, (6,), name_type_map['Ubyte']), (False, None)
