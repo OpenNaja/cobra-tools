f3940fe9fca2f11e589220a285192083ae966c79
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-06-14 13:04:36 +0200

    Improve manis

--- generated/formats/manis/compounds/FloatsGrabber.py
+++ generated/formats/manis/compounds/FloatsGrabber.py
@@ -34,14 +34,13 @@ class FloatsGrabber(BaseStruct):
 		num_bounds = np.max(instance.arg) + 1
 		instance.mins = Array.from_stream(stream, instance.context, 0, None, (num_bounds, 3), Float)
 		instance.scales = Array.from_stream(stream, instance.context, 0, None, (num_bounds, 3), Float)
-		logging.debug(f"Compressed keys data ends at {stream.tell()}")
 		instance.io_size = stream.tell() - instance.io_start
 
 	@classmethod
 	def write_fields(cls, stream, instance):
 		instance.io_start = stream.tell()
-		for bound in instance.bounds:
-			LocBound.to_stream(bound, stream, instance.context)
+		Array.to_stream(instance.mins, stream, instance.context, 0, None, instance.mins.shape, Float)
+		Array.to_stream(instance.scales, stream, instance.context, 0, None, instance.scales.shape, Float)
 		instance.io_size = stream.tell() - instance.io_start
 
 	@classmethod
