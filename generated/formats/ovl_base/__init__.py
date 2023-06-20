dda8d54f2203d85840cdb6c22994ad4caf6e3d9d
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-06-20 18:40:04 +0200

    Init is_biosyn on context

--- generated/formats/ovl_base/__init__.py
+++ generated/formats/ovl_base/__init__.py
@@ -5,6 +5,7 @@ from generated.formats.ovl_base.bitfields.VersionInfo import VersionInfo
 class OvlContext(object):
 	def __init__(self):
 		self.version = 0
+		self.is_biosyn = 0
 		self.user_version = VersionInfo()
 
 	def __repr__(self):
