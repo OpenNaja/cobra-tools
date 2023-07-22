fbef14e9c749e4e895c5b000ce6de5d91b59a7ca
Jon <170077+hexabits@users.noreply.github.com>
2023-07-22 11:39:05 -0400

    Add SUCCESS logging level

--- ovl_util/config.py
+++ ovl_util/config.py
@@ -103,6 +103,7 @@ class ColoredFormatter(logging.Formatter):
 		self.FORMATS = {
 			logging.DEBUG: f"{ANSI.DARK_GRAY}{self._fmt}{ANSI.END}",
 			logging.INFO: f"{ANSI.LIGHT_WHITE}{self._fmt}{ANSI.END}",
+			logging.INFO + 5: f"{ANSI.LIGHT_GREEN}{self._fmt}{ANSI.END}", # SUCCESS
 			logging.WARNING: f"{ANSI.YELLOW}{self._fmt}{ANSI.END}",
 			logging.ERROR: f"{ANSI.RED}{self._fmt}{ANSI.END}",
 			logging.CRITICAL: f"{ANSI.LIGHT_RED}{self._fmt}{ANSI.END}"
@@ -114,8 +115,59 @@ class ColoredFormatter(logging.Formatter):
 		return formatter.format(record)
 
 
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present 
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    """
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+       raise AttributeError('{} already defined in logging module'.format(levelName))
+    if hasattr(logging, methodName):
+       raise AttributeError('{} already defined in logging module'.format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+       raise AttributeError('{} already defined in logger class'.format(methodName))
+
+    # This method was inspired by the answers to Stack Overflow post
+    # http://stackoverflow.com/q/2183233/2988730, especially
+    # http://stackoverflow.com/a/13638084/2988730
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
+
+
 def logging_setup(log_name):
 	log_path = f'{os.path.join(root_dir, log_name)}.log'
+	addLoggingLevel('SUCCESS', logging.INFO + 5)
 	logger = logging.getLogger()
 	logger.setLevel(logging.DEBUG)
 	# formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
