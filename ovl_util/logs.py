3e6ff041d926d9d8d3a0e5ed04cbc603bb74c64e
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-07-28 14:13:41 +0200

    Cleanup logs

--- ovl_util/logs.py
+++ ovl_util/logs.py
@@ -117,17 +117,6 @@ def addLoggingLevel(levelName, levelNum, methodName=None):
 	if hasattr(logging.getLoggerClass(), methodName):
 		raise AttributeError(f"{methodName} already defined in logger class.")
 
-	# This method was inspired by the answers to Stack Overflow post
-	# http://stackoverflow.com/q/2183233/2988730, especially
-	# http://stackoverflow.com/a/13638084/2988730
-	def logForLevel(self, message, *args, **kwargs):
-		if self.isEnabledFor(levelNum):
-			self._log(levelNum, message, args, **kwargs)
-	def logToRoot(message, *args, **kwargs):
-		if logging.root.isEnabledFor(levelNum):
-			logging.root._log(levelNum, message, args, **kwargs)
-		# logging.log(levelNum, message, *args, **kwargs)
-
 	logging.addLevelName(levelNum, levelName)
 	setattr(logging, levelName, levelNum)
 	setattr(logging.getLoggerClass(), methodName, partialmethod(logging.getLoggerClass().log, levelNum))
