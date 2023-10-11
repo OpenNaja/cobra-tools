350c9ea12dbaba129e8b458b2d47ac879e7e487b
ilodev <ilodev@users.noreply.github.com>
2023-10-11 09:29:57 +0200

    enabling .curve

--- constants/Jurassic World Evolution 2/mimes_extra.py
+++ constants/Jurassic World Evolution 2/mimes_extra.py
@@ -2,4 +2,5 @@ from constants import Mime
 
 mimes = {
 	'.materialcollection': Mime(name='Casino:MaterialCollection:materialcollection', hash=2350825847, version=1, triplets=[], pool=4, set_pool=0),
+	'.curve': Mime(name='Casino:AnimationCurveData:curve', hash=2961744133, version=1, triplets=[(1, 2, 3)], pool=4, set_pool=0),
 }
