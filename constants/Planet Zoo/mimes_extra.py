3c7b9c8980874b9d32d1baae9baaa1d3b45a0b23
ilodev <ilodev@users.noreply.github.com>
2023-10-11 17:15:09 +0200

    removing a leftover on the extra mimes

--- constants/Planet Zoo/mimes_extra.py
+++ constants/Planet Zoo/mimes_extra.py
@@ -1,6 +1,5 @@
 from constants import Mime
 
 mimes = {
-	'.materialcollection': Mime(name='Casino:MaterialCollection:materialcollection', hash=2350825847, version=1, triplets=[], pool=4, set_pool=0),
 	'.curve': Mime(name='Casino:AnimationCurveData:curve', hash=2961744133, version=1, triplets=[], pool=4, set_pool=0),
 }
