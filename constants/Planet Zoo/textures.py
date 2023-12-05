c7264fcd51b3cdd654eb9ee1d17251b7e04688fb
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-12-05 22:52:11 +0100

    plugin - add PZ foliage texture preset

--- constants/Planet Zoo/textures.py
+++ constants/Planet Zoo/textures.py
@@ -8,4 +8,16 @@ textures = {
 			"pRoughnessPackedTexture": {"_MT": "_R", "_SP": "_G", "_RN": "_B", "_FO": "_A"},
 			"pFlexiColourMaskTexture": {"_F1": "_R", "_F2": "_G", "_F3": "_B", "_F4": "_A"},
 		},
+	'Foliage_Clip': {
+			"pBaseColourTexture": {"_BC": "_RGB", "": "_A"},  # A apparently unused
+			"pAOTexture": {"_AO": ""},
+			"pNormalTexture": {"_NM": "_RGB"},  # BC5_UNORM
+			"pRoughnessPackedTexture": {"_MT": "_R", "_SP": "_G", "_RN": "_B", "_OP": "_A"},
+		},
+	'Foliage_Opaque': {
+			"pBaseColourTexture": {"_BC": "_RGB", "": "_A"},  # A apparently unused
+			"pAOTexture": {"_AO": ""},
+			"pNormalTexture": {"_NM": "_RGB"},  # BC5_UNORM
+			"pRoughnessPackedTexture": {"_MT": "_R", "_SP": "_G", "_RN": "_B", "_OP": "_A"},  # a is unused
+		},
 }
