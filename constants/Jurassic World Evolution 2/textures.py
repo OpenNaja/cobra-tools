e4633882cdf13967b9d9651a656bf64089701137
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-12-05 22:41:10 +0100

    plugin - add JWE2 foliage texture preset

--- constants/Jurassic World Evolution 2/textures.py
+++ constants/Jurassic World Evolution 2/textures.py
@@ -1,11 +1,21 @@
 textures = {
 	'Metallic_Roughness_Opaque_Emissive': {
-			# JWE2 trees seem to have CA on tree trunks pBaseColourTexture alpha, not on tree leaves
-			# CA apparently not used on trunks in PZ (trunk has AO maps instead)
-			"pBaseColourTexture": {"_BC": "_RGB", "_CA": "_A"},
+			"pBaseColourTexture": {"_BC": "_RGB", "_CA": "_A"},  # check if A is used here
 			"pEmissiveTexture": {"_EM": ""},
 			"pNormalTexture": {"_NM": "_RGB", "_AO": "_A"},  # BC7_UNORM - no coord dropping?
 			"pRoughnessPackedTexture": {"_MT": "_R", "_SP": "_G", "_RN": "_B", "_FO": "_A"},
 			"pFlexiColourMaskTexture": {"_F1": "_R", "_F2": "_G", "_F3": "_B", "_F4": "_A"},
+		},
+	'Foliage_Clip': {
+			"pBaseColourTexture": {"_BC": "_RGB", "_CA": "_A"},  # not on tree leaves
+			"pNormalTexture": {"_NM": "_RGB", "_AO": "_A"},  # BC7_UNORM - no coord dropping?
+			"pRoughnessPackedTexture": {"_OP": "_R", "_RN": "_G", "_SP": "_B", "_MT": "_A"},
+		},
+	'Foliage_Opaque': {
+			"pBaseColourTexture": {"_BC": "_RGB", "_CA": "_A"},  # not sure what A is
+			"pNormalTexture": {"_NM": "_RGB", "_AO": "_A"},  # not sure if A is actually AO
+			"pMoiseNoiseMask": {},  # ?
+			"pMossBaseColour": {},  # ?
+			"pMossNormalTexture": {},  # ?
 		},
 }
