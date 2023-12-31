d3bac08fe90bf3b7df210b24a9a9c6213a35537c
HENDRIX-ZT2 <arnfinn@spholderer.de>
2023-12-31 12:06:22 +0100

    supportset - add Pillar.fln_model pointer

--- generated/formats/path/compounds/Pillar.py
+++ generated/formats/path/compounds/Pillar.py
@@ -10,11 +10,11 @@ class Pillar(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.unk_int = name_type_map['Uint64'](self.context, 0, None)
 		self.unk_floats = Array(self.context, 0, None, (0,), name_type_map['Float'])
 		self.unk_int_2 = name_type_map['Uint64'](self.context, 0, None)
 		self.pillar_model = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.cap_model = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.fln_model = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
 			self.set_defaults()
 
@@ -23,7 +23,7 @@ class Pillar(MemStruct):
 		yield from super()._get_attribute_list()
 		yield 'pillar_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'cap_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'unk_int', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'fln_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'unk_floats', Array, (0, None, (2,), name_type_map['Float']), (False, None), (None, None)
 		yield 'unk_int_2', name_type_map['Uint64'], (0, None), (False, None), (None, None)
 
@@ -32,6 +32,6 @@ class Pillar(MemStruct):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'pillar_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'cap_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'unk_int', name_type_map['Uint64'], (0, None), (False, None)
+		yield 'fln_model', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'unk_floats', Array, (0, None, (2,), name_type_map['Float']), (False, None)
 		yield 'unk_int_2', name_type_map['Uint64'], (0, None), (False, None)
