2397ee4d7dd70f48576f1ef8f195ea4f89d91e6e
Jon <170077+hexabits@users.noreply.github.com>
2024-01-14 08:57:36 -0500

    PZ 1.16 FGM Inspection

--- constants/Planet Zoo/shaders_extra.py
+++ constants/Planet Zoo/shaders_extra.py
@@ -3,6 +3,33 @@
 # Note: Statistics may not be accurate due to manual attribute compilation
 
 shaders = {
+	'Game_Terrain_Clip': (
+		[
+			'pCavityRoughnessDielectricArray',
+			'pCoarseNoise',
+			'pDiffuseArray',
+			'pGradHeightArray',
+			'pMacroDiffuse',
+		],
+		{
+			'pCoarseNoiseScale': (0, [((1.0,), 1)]),
+			'pCoarseNoiseTile': (0, [((1.0,), 1)]),
+			'pEnableScreenSpaceAO': (6, [((1,), 1)]),
+			'pEnabledNewHeightBlending': (6, [((0,), 1)]),
+			'pFallbackCavityRoughness': (1, [((1.0, 1.0), 1)]),
+			'pFallbackSpecular': (2, [((0.0, 0.0, 0.0), 1)]),
+			'pGrassRoughness': (0, [((0.2,), 1)]),
+			'pGrassiness': (0, [((0.667,), 1)]),
+			'pHeightBlendingDistance': (0, [((0.2,), 1)]),
+			'pHeightBlendingScale': (0, [((1.0,), 1)]),
+			'pLayerHeightBlend': (0, [((0.1,), 1)]),
+			'pMacroTile': (0, [((0.1,), 1)]),
+			'pRenderLayerOverride': (5, [((-1,), 1)]),
+			'pSnowOnSlopesOffset': (0, [((0.0,), 1)]),
+			'pWeather_ExplicitNormalThreshold': (0, [((0.85,), 1)]),
+			'pWeather_LocalWaterAmount': (0, [((1.0,), 1)]),
+		}
+	),
 	'Metallic_Roughness_Clip_Weather_Transmissive': (
 		[
 			'pAOTexture',
@@ -34,6 +61,21 @@ shaders = {
 			'pVerticalTiling': (0, [((0.25,), 10)]),
 			'pWeather_Enable': (6, [((1,), 10)]),
 			'pWeather_ExplicitNormalThreshold': (0, [((0.85,), 10)]),
+		}
+	),
+	'RenderMesh_WhiteWorldUI': (
+		[
+		],
+		{
+			'pRenderLayerOverride': (5, [((-1,), 1)]),
+		}
+	),
+	'RenderMesh_WhiteWorldUI_FromTexture': (
+		[
+			'pPropertyTexture',
+		],
+		{
+			'pRenderLayerOverride': (5, [((-1,), 1)]),
 		}
 	),
 }
