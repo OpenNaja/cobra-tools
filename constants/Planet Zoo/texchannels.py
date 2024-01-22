62d32e32a683713960617a568c4983c94d3239f3
Jon <170077+hexabits@users.noreply.github.com>
2024-01-22 11:20:30 -0500

    Update marking definitions

--- constants/Planet Zoo/texchannels.py
+++ constants/Planet Zoo/texchannels.py
@@ -1,6 +1,6 @@
 texchannels = {
-	'p2MarkingBaldnessScarTexture': {"R": "M1", "G": "M2", "B": "BA", "A": "SC"},
-	'p3MarkingScarTexture': {"R": "M1", "G": "M2", "B": "M3", "A": "SC"},
+	'p2MarkingBaldnessScarTexture': {"R": "PW_M1", "G": "PW_M2", "B": "BA", "A": "SC"},
+	'p3MarkingScarTexture': {"R": "PW_M1", "G": "PW_M2", "B": "PW_M3", "A": "SC"},
 	'pAOTexture': {"": "AO"},
 	'pAOTexture2': {"": "AO2"},  # Blend between AO and AO2
 	'pAOTextureDetail': {"": "AO2"},  # Overlaid on pAOTextureUnique
@@ -49,8 +49,8 @@ texchannels = {
 	'pLeucisticBaseColourAndMaskTexture': {"RGB": "LE", "A": "LE_HM"},
 	'pMacroDamageBlendMap': {"": ""},
 	'pMacroDiffuse': {"": ""},
-	'pMarkingNoise': {"": ""},
-	'pMarkingPatchworkMask': {"": ""},  # Lookup/Input Mask
+	'pMarkingNoise': {"": "MN"},
+	'pMarkingPatchworkMask': {"": "PW"},  # Lookup/Input Mask
 	'pMaskAOTextureUnique': {"R": "DM", "G": "AO"},
 	'pMelanisticBaseColourAndMaskTexture': {"RGB": "ME", "A": "ME_HM"},
 	'pMossBaseColourRoughnessPackedTexture': {"RGB": "BC", "A": "RN"},
@@ -66,8 +66,8 @@ texchannels = {
 	'pNormalTextureUnique': {"RG": "NM"},  # Same as Detail_Basic pNormalMapTextureUnique but for Detail
 	'pOpacityTexture': {"": "OP"},
 	'pOpacityTexture2': {"": "OP2"},  # Blend between OP and OP2
-	'pPiebaldMask': {"": ""},  # Lookup/Input Mask
-	'pPiebaldTexture': {"R": "", "G": "", "B": "", "A": ""},
+	'pPiebaldMask': {"": "PB"},  # Lookup/Input Mask
+	'pPiebaldTexture': {"R": "PB_M1", "G": "PB_M2", "B": "PB_M3", "A": ""},
 	# 'pPropertyTexture': {"": ""},
 	'pRetroreflectiveness': {"": ""},
 	'pRoughnessAOPackedTextureDetailBase': {"R": "RN", "G": "AO"},
