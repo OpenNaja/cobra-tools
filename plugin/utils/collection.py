b5dad112f047a74fce06ddd1a9853f8863d22f42
HENDRIX-ZT2 <arnfinn@spholderer.de>
2024-02-03 15:54:20 +0100

    plugin - #368 improve share materials

--- plugin/utils/collection.py
+++ plugin/utils/collection.py
@@ -5,43 +5,41 @@ from collections import defaultdict
 # https://blender.stackexchange.com/questions/157828/how-to-duplicate-a-certain-collection-using-python
 
 
-def copy_objects(from_col, to_col, linked, dupe_lut, replacer, mat_lut):
-	for o in from_col.objects:
-		dupe = o.copy()
-		dupe.name = replacer(o.name)
-		if not linked and o.data:
+def copy_objects(from_col, to_col, linked, dupe_lut, replacer, share_materials):
+	for ob in from_col.objects:
+		dupe = ob.copy()
+		dupe.name = replacer(ob.name)
+		if not linked and ob.data:
 			dupe.data = dupe.data.copy()
-			dupe.data.name = replacer(dupe.data.name)
+			dupe.data.name = replacer(ob.data.name)
 			# use LUT for mats to only duplicate once per run
-			if dupe.type == "MESH" and mat_lut is not None:
+			if dupe.type == "MESH" and not share_materials:
 				for i, mat in enumerate(dupe.data.materials):
-					# fallback copy_name if replacer changes nothing
-					new_name = replacer(mat.name)
-					if new_name != mat.name:
-						name_key = new_name
-					else:
-						name_key = f"{mat.name}_copy"
-					# find or store previously copied material
-					if name_key in mat_lut:
-						copy_mat = mat_lut[name_key]
+					# find previously copied material
+					if mat in dupe_lut:
+						copy_mat = dupe_lut[mat]
 					else:
 						copy_mat = mat.copy()
-						copy_mat.name = name_key
-						mat_lut[name_key] = copy_mat
-					# update slot
+						# fallback to name_copy if replacer changes nothing
+						new_name = replacer(mat.name)
+						if new_name != mat.name:
+							copy_mat.name = new_name
+						else:
+							copy_mat.name = f"{mat.name}_copy"
+						dupe_lut[mat] = copy_mat
+					# update material slot
 					dupe.data.materials[i] = copy_mat
 		to_col.objects.link(dupe)
-		dupe_lut[o] = dupe
+		dupe_lut[ob] = dupe
 
 
 def copy(parent, collection, linked=False, replacer=None, share_materials=True):
 	assert parent != collection
 	dupe_lut = defaultdict(lambda: None)
-	mat_lut = None if share_materials else {}
 
 	def _copy(parent, collection, linked=False):
 		cc = bpy.data.collections.new(replacer(collection.name))
-		copy_objects(collection, cc, linked, dupe_lut, replacer, mat_lut)
+		copy_objects(collection, cc, linked, dupe_lut, replacer, share_materials)
 
 		for c in collection.children:
 			_copy(cc, c, linked)
@@ -49,8 +47,10 @@ def copy(parent, collection, linked=False, replacer=None, share_materials=True):
 		parent.children.link(cc)
 
 	_copy(parent, collection, linked)
-	# print(dupe_lut)
-	for o, dupe in tuple(dupe_lut.items()):
-		parent = dupe_lut[o.parent]
-		if parent:
-			dupe.parent = parent
+
+	# fix parenting to use a duped parent instead
+	for ob, dupe in tuple(dupe_lut.items()):
+		if isinstance(ob, bpy.types.Object):
+			parent = dupe_lut[ob.parent]
+			if parent:
+				dupe.parent = parent
