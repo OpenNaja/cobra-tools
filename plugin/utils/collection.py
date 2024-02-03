1189982ba676ab4e84593bbc006cf93f852d658f
HENDRIX-ZT2 <arnfinn@spholderer.de>
2024-02-03 15:36:22 +0100

    plugin - #368 option for whether to duplicate or share materials

--- plugin/utils/collection.py
+++ plugin/utils/collection.py
@@ -5,19 +5,30 @@ from collections import defaultdict
 # https://blender.stackexchange.com/questions/157828/how-to-duplicate-a-certain-collection-using-python
 
 
-def copy_objects(from_col, to_col, linked, dupe_lut, replacer, share_materials=True):
+def copy_objects(from_col, to_col, linked, dupe_lut, replacer, mat_lut):
 	for o in from_col.objects:
 		dupe = o.copy()
 		dupe.name = replacer(o.name)
 		if not linked and o.data:
 			dupe.data = dupe.data.copy()
 			dupe.data.name = replacer(dupe.data.name)
-			# todo logic of share_materials appears inverted
-			# todo also make sure to only duplicate once per run
-			if hasattr(dupe.data, "materials") and share_materials:
+			# use LUT for mats to only duplicate once per run
+			if dupe.type == "MESH" and mat_lut is not None:
 				for i, mat in enumerate(dupe.data.materials):
-					copy_mat = mat.copy()
-					copy_mat.name = replacer(mat.name)
+					# fallback copy_name if replacer changes nothing
+					new_name = replacer(mat.name)
+					if new_name != mat.name:
+						name_key = new_name
+					else:
+						name_key = f"{mat.name}_copy"
+					# find or store previously copied material
+					if name_key in mat_lut:
+						copy_mat = mat_lut[name_key]
+					else:
+						copy_mat = mat.copy()
+						copy_mat.name = name_key
+						mat_lut[name_key] = copy_mat
+					# update slot
 					dupe.data.materials[i] = copy_mat
 		to_col.objects.link(dupe)
 		dupe_lut[o] = dupe
@@ -26,10 +37,11 @@ def copy_objects(from_col, to_col, linked, dupe_lut, replacer, share_materials=T
 def copy(parent, collection, linked=False, replacer=None, share_materials=True):
 	assert parent != collection
 	dupe_lut = defaultdict(lambda: None)
+	mat_lut = None if share_materials else {}
 
 	def _copy(parent, collection, linked=False):
 		cc = bpy.data.collections.new(replacer(collection.name))
-		copy_objects(collection, cc, linked, dupe_lut, replacer, share_materials)
+		copy_objects(collection, cc, linked, dupe_lut, replacer, mat_lut)
 
 		for c in collection.children:
 			_copy(cc, c, linked)
