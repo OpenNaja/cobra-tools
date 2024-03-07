1022e733f10a3cd8a9e50701e6ff196e73e373e9
Jon <170077+hexabits@users.noreply.github.com>
2024-03-07 02:17:12 -0500

    Update pz_animals.sql

--- sql_commands/pz_animals.sql
+++ sql_commands/pz_animals.sql
@@ -1,13 +1,8 @@
 -- FDB Animals
 --***********************
 
--- IF USING IN SQLITESTUDIO:
--- Open with Ctrl+O or the Folder icon in SQL Editor
--- Make sure Configuration > SQL Queries > "Execute only the query under the cursor" is UNCHECKED
--- Replace the 2 strings below Original/New with your base and modded species
--- NOTES:
---    Incomplete names ARE supported e.g. 'Grey' -> 'Harbor' will rename GreySeal to HarborSeal
---    Do NOT use incomplete strings that are too short or generic e.g. Giant, Red, Nile, which all have multiple species matched.
+-- NOT FOR MANUAL USE
+-- Please see #pz-modding-resources in the modding discord for manual scripts
 
 CREATE TEMP TABLE IF NOT EXISTS Replacement(Original TEXT PRIMARY KEY, New TEXT);
 INSERT OR IGNORE INTO Replacement(Original,				New)
@@ -32,8 +27,10 @@ CREATE TABLE IF NOT EXISTS GimmickMovementData (Species TEXT PRIMARY KEY REFEREN
 CREATE TABLE IF NOT EXISTS LakeSurfaceFollowerData (AnimalType STRING REFERENCES AnimalDefinitions (AnimalType) ON UPDATE CASCADE PRIMARY KEY UNIQUE NOT NULL COLLATE NOCASE, MaleSwimHeightOffset REAL DEFAULT (0), MaleWadeHeightOffset REAL DEFAULT (0), MaleUseWadeDepth BOOLEAN DEFAULT (0), MaleWadeDepth REAL DEFAULT (0), FemaleSwimHeightOffset REAL DEFAULT (0), FemaleWadeHeightOffset REAL DEFAULT (0), FemaleUseWadeDepth BOOLEAN DEFAULT (0), FemaleWadeDepth REAL DEFAULT (0), JuvenileSwimHeightOffset REAL DEFAULT (0), JuvenileWadeHeightOffset REAL DEFAULT (0), JuvenileUseWadeDepth BOOLEAN DEFAULT (0), JuvenileWadeDepth REAL DEFAULT (0));
 CREATE TABLE IF NOT EXISTS MaturationEnum (MaturationType TEXT PRIMARY KEY UNIQUE NOT NULL, EnumValue INTEGER UNIQUE NOT NULL);
 CREATE TABLE IF NOT EXISTS SocialityData (AnimalType TEXT PRIMARY KEY REFERENCES AnimalDefinitions (AnimalType) ON UPDATE CASCADE UNIQUE NOT NULL, GroupNameLoc TEXT NOT NULL DEFAULT Generic, MaturationType TEXT REFERENCES MaturationEnum (MaturationType) NOT NULL, RelationsDecayTimer DOUBLE NOT NULL DEFAULT (0.0), ChanceToBondOnSocialTheatre DOUBLE DEFAULT (0.0) NOT NULL);
+-- 1.15 Tables
+CREATE TABLE IF NOT EXISTS ViewingDomeParticipantData (AnimalType TEXT REFERENCES AnimalDefinitions (AnimalType) ON UPDATE CASCADE NOT NULL COLLATE NOCASE, DoActionAtDomeWeight DOUBLE CHECK (DoActionAtDomeWeight <= 1.0 AND DoActionAtDomeWeight >= 0.0) NOT NULL DEFAULT (0.0), GoToDomeWeight DOUBLE CHECK (GoToDomeWeight <= 1.0 AND GoToDomeWeight >= 0.0) NOT NULL DEFAULT (1.0));
 
-/* This clears out everything except our base creature */
+
 DELETE FROM ActionCalculationParameters WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM AnimalBiomePreferences WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM AnimalBurrowsData WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
@@ -104,6 +101,7 @@ DELETE FROM SpeciesWithAlpha WHERE AnimalType NOT LIKE ('%'||(SELECT Original FR
 DELETE FROM StressParameters WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM TheatreClearRadii WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM ViewingDistanceModifier WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
+DELETE FROM ViewingDomeParticipantData WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM VisualVariation WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM EnrichmentPartialTypesToUse WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 DELETE FROM AudioPerActionCallData WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%'); 
@@ -113,7 +111,7 @@ DELETE FROM WaterStationSuitabilityBlacklist WHERE AnimalType NOT LIKE ('%'||(SE
 DELETE FROM SocialEnrichmentData;
 DELETE FROM AnimalDefinitions WHERE AnimalType NOT LIKE ('%'||(SELECT Original FROM Replacement)||'%') AND AnimalType NOT LIKE ('%'||(SELECT New FROM Replacement)||'%');
 
-/* This creates our new creature modeled off the base creature in the master table for this FDB */
+
 INSERT OR IGNORE INTO AnimalDefinitions (AnimalType,AdultMaleGamePrefab,AdultMaleVisualPrefab,AdultFemaleGamePrefab,AdultFemaleVisualPrefab,JuvenileGamePrefab,JuvenileVisualPrefab,ContentPack)
 SELECT
     replace(AnimalType, (SELECT Original FROM Replacement), (SELECT New FROM Replacement)),
@@ -126,7 +124,7 @@ SELECT
     ContentPack
 FROM AnimalDefinitions;
 
-/* This updates all other tables to use your new species instead of the base creature */
+
 UPDATE ActionCalculationParameters SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE AnimalBiomePreferences SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE AnimalBurrowsData SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
@@ -196,6 +194,7 @@ UPDATE SpeciesWithAlpha SET AnimalType = (SELECT AnimalType FROM AnimalDefinitio
 UPDATE StressParameters SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE TheatreClearRadii SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE ViewingDistanceModifier SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
+UPDATE ViewingDomeParticipantData SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE VisualVariation SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE EnrichmentPartialTypesToUse SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE FixRagdollMethodData SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
@@ -203,7 +202,7 @@ UPDATE AudioPerActionCallData SET AnimalType = (SELECT AnimalType FROM AnimalDef
 UPDATE TemporaryFoodVisualOffsetModifier SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 UPDATE WaterStationSuitabilityBlacklist SET AnimalType = (SELECT AnimalType FROM AnimalDefinitions WHERE AnimalType LIKE '%'||(SELECT New FROM Replacement)||'%') WHERE AnimalType LIKE '%'|| (SELECT Original FROM Replacement) ||'%';
 
-/* This clears out the base creature. */
+
 DELETE FROM AnimalDefinitions WHERE AnimalType NOT LIKE replace(AnimalType, (SELECT Original FROM Replacement), (SELECT New FROM Replacement));
 
 DROP TABLE Replacement;
