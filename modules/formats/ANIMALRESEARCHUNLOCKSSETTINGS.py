01612939a8441b21ec1650d109c593103d757e4c
HENDRIX-ZT2 <arnfinn@spholderer.de>
2024-03-11 18:37:03 +0100

    research - clean up + fix inject

--- modules/formats/ANIMALRESEARCHUNLOCKSSETTINGS.py
+++ modules/formats/ANIMALRESEARCHUNLOCKSSETTINGS.py
@@ -7,20 +7,6 @@ class AnimalresearchunlockssettingsLoader(MemStructLoader):
 	target_class = ResearchRoot
 	extension = ".animalresearchunlockssettings"
 
-	def prep(self):
-		# avoid generating pointers for these
-		for level in self.header.levels.data:
-			if not level.next_level_count:
-				level.next_levels.data = None
-			if not level.children_count:
-				level.children.data = None
-
-	def create(self, file_path):
-		self.header = self.target_class.from_xml_file(file_path, self.ovl.context)
-		# print(self.header)
-		# self.prep()
-		self.write_memory_data()
-
 
 class AnimalresearchstartunlockedssettingsLoader(MemStructLoader):
 	target_class = ResearchStartRoot
--- modules/formats/MECHANICRESEARCHSETTINGS.py
+++ modules/formats/MECHANICRESEARCHSETTINGS.py
@@ -5,16 +5,3 @@ from modules.formats.BaseFormat import MemStructLoader
 class MechanicresearchsettingsLoader(MemStructLoader):
 	target_class = ResearchRoot
 	extension = ".mechanicresearchsettings"
-
-	def prep(self):
-		# avoid generating pointers for these
-		for research in self.header.levels.data:
-			if not research.next_research_count:
-				research.next_research.data = None
-
-	def create(self, file_path):
-		self.header = self.target_class.from_xml_file(file_path, self.ovl.context)
-		self.prep()
-		self.write_memory_data()
-
-
