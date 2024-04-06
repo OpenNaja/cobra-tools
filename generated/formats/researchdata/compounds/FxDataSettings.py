db2c38886c6f0e9ad1cc6bb1b58f03cf6ce37eea
cx-koraka <40576336+cx-koraka@users.noreply.github.com>
2024-04-06 11:35:26 +0100

    ammend researchdata to not reuse pointer name

--- generated/formats/researchdata/compounds/FxDataSettings.py
+++ generated/formats/researchdata/compounds/FxDataSettings.py
@@ -17,7 +17,8 @@ class FxDataSettings(MemStruct):
 		self.unk_5 = name_type_map['Uint64'](self.context, 0, None)
 		self.unk_6 = name_type_map['Uint64'](self.context, 0, None)
 		self.unk_7 = name_type_map['Uint64'](self.context, 0, None)
-		self.unk_ptr = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.unk_ptr_0 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.unk_ptr_1 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
 			self.set_defaults()
 
@@ -25,8 +26,8 @@ class FxDataSettings(MemStruct):
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
 		yield 'unk_0', name_type_map['Uint64'], (0, None), (False, None), (None, None)
-		yield 'unk_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'unk_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'unk_ptr_0', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'unk_ptr_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'unk_1', name_type_map['Uint64'], (0, None), (False, None), (None, None)
 		yield 'unk_2', name_type_map['Uint64'], (0, None), (False, None), (None, None)
 		yield 'unk_3', name_type_map['Uint64'], (0, None), (False, None), (None, None)
@@ -39,8 +40,8 @@ class FxDataSettings(MemStruct):
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'unk_0', name_type_map['Uint64'], (0, None), (False, None)
-		yield 'unk_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'unk_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'unk_ptr_0', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'unk_ptr_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'unk_1', name_type_map['Uint64'], (0, None), (False, None)
 		yield 'unk_2', name_type_map['Uint64'], (0, None), (False, None)
 		yield 'unk_3', name_type_map['Uint64'], (0, None), (False, None)
