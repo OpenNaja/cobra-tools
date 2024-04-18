dbfb8bceaa5f0ee96d2e4cf6e5bbe0142274aa58
ilodev <ilodev@users.noreply.github.com>
2024-04-18 16:45:11 +0200

    AssetPackage flag found
    this flag exposes the resources of the ovl to the game and the assetpkg doesn't need to be added individually to all prefabs once it has been loaded (e.g. reusing .tex from other ovls)

--- generated/formats/assetpkg/compounds/AssetpkgRoot.py
+++ generated/formats/assetpkg/compounds/AssetpkgRoot.py
@@ -9,7 +9,7 @@ class AssetpkgRoot(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self._zero = name_type_map['Uint64'](self.context, 0, None)
+		self.shared = name_type_map['Uint64'](self.context, 0, None)
 		self.asset_path = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
 			self.set_defaults()
@@ -18,10 +18,10 @@ class AssetpkgRoot(MemStruct):
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
 		yield 'asset_path', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield '_zero', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'shared', name_type_map['Uint64'], (0, None), (False, None), (None, None)
 
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'asset_path', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield '_zero', name_type_map['Uint64'], (0, None), (False, None)
+		yield 'shared', name_type_map['Uint64'], (0, None), (False, None)
