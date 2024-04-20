916960c1834221d2337e1f9dc4de870520cb5a38
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-04-20 21:09:43 +0200

    particleeffect - improve effects 7, 8, 12

--- generated/formats/particleeffect/compounds/Effect08.py
+++ generated/formats/particleeffect/compounds/Effect08.py
@@ -16,9 +16,7 @@ class Effect08(Effect):
 		super().__init__(context, arg, template, set_default=False)
 		self.floats = Array(self.context, 0, None, (0,), name_type_map['Float'])
 		self.minus_1 = name_type_map['Int'](self.context, 0, None)
-		self.z_2 = name_type_map['Int'](self.context, 0, None)
-		self.z_3 = name_type_map['Int'](self.context, 0, None)
-		self.pi = name_type_map['Float'](self.context, 0, None)
+		self.angle = Array(self.context, 0, None, (0,), name_type_map['Float'])
 		self.floats_2 = Array(self.context, 0, None, (0,), name_type_map['Float'])
 		if set_default:
 			self.set_defaults()
@@ -28,9 +26,7 @@ class Effect08(Effect):
 		yield from super()._get_attribute_list()
 		yield 'floats', Array, (0, None, (15,), name_type_map['Float']), (False, None), (None, None)
 		yield 'minus_1', name_type_map['Int'], (0, None), (False, None), (None, None)
-		yield 'z_2', name_type_map['Int'], (0, None), (False, None), (None, None)
-		yield 'z_3', name_type_map['Int'], (0, None), (False, None), (None, None)
-		yield 'pi', name_type_map['Float'], (0, None), (False, None), (None, None)
+		yield 'angle', Array, (0, None, (3,), name_type_map['Float']), (False, None), (None, None)
 		yield 'floats_2', Array, (0, None, (5,), name_type_map['Float']), (False, None), (None, None)
 
 	@classmethod
@@ -38,7 +34,5 @@ class Effect08(Effect):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'floats', Array, (0, None, (15,), name_type_map['Float']), (False, None)
 		yield 'minus_1', name_type_map['Int'], (0, None), (False, None)
-		yield 'z_2', name_type_map['Int'], (0, None), (False, None)
-		yield 'z_3', name_type_map['Int'], (0, None), (False, None)
-		yield 'pi', name_type_map['Float'], (0, None), (False, None)
+		yield 'angle', Array, (0, None, (3,), name_type_map['Float']), (False, None)
 		yield 'floats_2', Array, (0, None, (5,), name_type_map['Float']), (False, None)
