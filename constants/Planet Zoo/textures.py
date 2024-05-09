b05673f60cbf37f7971a0d8328597429c0101bef
Jon <170077+hexabits@users.noreply.github.com>
2024-05-09 09:00:53 -0400

    Blender - Fix Metallic_Roughness_Opaque_BC7

--- constants/Planet Zoo/textures.py
+++ constants/Planet Zoo/textures.py
@@ -1,12 +1,12 @@
 textures = {
 	'Metallic_Roughness_Opaque_EmissiveLightType_Weather': {
-			# decide which pRoughnessPackedTexture is the default
-			"pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "FO"},
-		},
+		# decide which pRoughnessPackedTexture is the default
+		"pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "FO"},
+	},
 	'Foliage_Clip': {
-			# "pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "OP"},
-		},
+		# "pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "OP"},
+	},
 	'Foliage_Opaque': {
-			# "pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "OP"},  # a is unused
-		},
+		# "pRoughnessPackedTexture": {"R": "MT", "G": "SP", "B": "RN", "A": "OP"},  # a is unused
+	},
 }
