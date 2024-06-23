16c36403f199f1d8e9545c0938fdf33b8ccd035a
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-06-23 15:05:15 +0200

    clean up auto_updater.py

--- ovl_util/auto_updater.py
+++ ovl_util/auto_updater.py
@@ -9,6 +9,9 @@ import time
 import logging
 import subprocess
 
+from root_path import root_dir
+from ovl_util.logs import ANSI
+
 # temporarily set to make sure it shows up, even though it is not written to the log file
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -17,11 +20,9 @@ logging.debug(f"Checking for automatic module updates")
 
 def install_prompt(question) -> bool:
     """ask question and return True if user confirms"""
-    print(question)
+    print(f"{ANSI.LIGHT_WHITE}{question}{ANSI.END} (y/N)")
     print(f"{ANSI.LIGHT_YELLOW}[Type y and hit Enter]{ANSI.END}{ANSI.LIGHT_GREEN}")
-    yes = {'yes', 'y', 'ye'}
-    choice = input().lower()
-    if choice in yes:
+    if input().lower() in {'yes', 'y', 'ye'}:
         return True
     else:
         return False
@@ -40,17 +41,15 @@ def pip_upgrade(package_name) -> int:
 
 
 try:
-    # pkg_resources and importlib.metadata are not available on py 3.7, 3.12
+    # pkg_resources is available on py <= 3.11
     from pkg_resources import packaging  # type: ignore
 except:
     logging.warning(f"pkg_resources is not available, can't upgrade dependencies")
     packaging = None
 try:
     from importlib import import_module
+    # importlib.metadata is available on py >= 3.8
     from importlib.metadata import distribution, PackageNotFoundError, packages_distributions
-
-    from root_path import root_dir
-    from ovl_util.logs import ANSI
 
     MISSING: dict[str, str] = {}
     OUTDATED: dict[str, str] = {}
@@ -64,32 +63,31 @@ try:
         pkg_dist = packages_distributions()
         for line in lines:
             lib, op, version = re.split("(~=|==|>=|<=|>|<)", line)
+            # Get import name from package name to verify it can be imported
+            for module, pkgs in pkg_dist.items():
+                if lib in pkgs:
+                    MODULES.append(module)
+            # check if module is installed
             try:
                 lib_dist = distribution(lib)
-                # Get import name from package name
-                for module, pkgs in pkg_dist.items():
-                    if lib in pkgs:
-                        MODULES.append(module)
-                # Check version
-                if packaging is not None:
-                    if packaging.version.parse(lib_dist.metadata['Version']) < packaging.version.parse(version):
-                        logging.warning(f"{lib} is out of date.")
-                        OUTDATED[lib] = line  # Need full line including ~= for pip install command
             except PackageNotFoundError:
-                logging.error(f"{lib} not found.")
+                logging.warning(f"{lib} is not installed.")
                 MISSING[lib] = line  # Need full line including ~= for pip install command
+                continue
+            # compare installed version to required version
+            if packaging is not None:
+                if packaging.version.parse(lib_dist.metadata['Version']) < packaging.version.parse(version):
+                    logging.warning(f"{lib} is out of date.")
+                    OUTDATED[lib] = line  # Need full line including ~= for pip install command
 
-    ASK_INSTALL = f"{ANSI.LIGHT_WHITE}Install the missing dependencies?{ANSI.END} (y/N)"
-    ASK_UPGRADE = f"{ANSI.LIGHT_WHITE}Update the outdated dependencies?{ANSI.END} (y/N)"
-
-    if len(MISSING) and install_prompt(ASK_INSTALL):
+    if MISSING and install_prompt("Install the missing dependencies?"):
         # upgrade pip then try installing the rest of packages
         pip_upgrade('pip')
         for key, package in MISSING.items():
             if pip_install(package) == 0:
                 INSTALLED.append(key)
 
-    if len(OUTDATED) and install_prompt(ASK_UPGRADE):
+    if OUTDATED and install_prompt("Update the outdated dependencies?"):
         # upgrade pip then try updating the outdated packages
         pip_upgrade('pip')
         for key, package in OUTDATED.items():
@@ -97,20 +95,19 @@ try:
                 UPDATED.append(key)
 
     if hasattr(logging, "success"):
-        if len(INSTALLED):
+        if INSTALLED:
             logging.success(f"Installed: {INSTALLED}")
 
-        if len(UPDATED):
+        if UPDATED:
             logging.success(f"Updated: {UPDATED}")
 
-    # Test all required modules can be imported
+    # verify that all found modules can be imported
     try:
         for module in MODULES:
             import_module(module)
     except:
-        logging.exception("Some modules could not be imported; make sure you install the required dependencies with pip!")
+        logging.exception("Some modules could not be imported; install the required dependencies with pip!")
         time.sleep(15)
 except:
     logging.exception("auto_updater didn't work")
-    import time
     time.sleep(15)
