8ae614fb2d3f20295659a034ba4cdb01a91b9db9
ilodev <ilodev@users.noreply.github.com>
2024-08-12 10:00:09 +0200

    fix for PC .pathresource

--- generated/formats/path/compounds/PathResource.py
+++ generated/formats/path/compounds/PathResource.py
@@ -13,6 +13,7 @@ class PathResource(MemStruct):
 		self.path_sub_type = name_type_map['Byte'](self.context, 0, None)
 		self.unk_byte_1 = name_type_map['Byte'].from_value(1)
 		self.unk_byte_2 = name_type_map['Byte'](self.context, 0, None)
+		self.pathtype = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.pathmaterial = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.pathextrusion_kerb = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.pathextrusion_railing = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
@@ -24,6 +25,7 @@ class PathResource(MemStruct):
 	@classmethod
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
+		yield 'pathtype', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: context.version == 18, None)
 		yield 'pathmaterial', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'pathextrusion_kerb', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'pathextrusion_railing', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
@@ -37,6 +39,8 @@ class PathResource(MemStruct):
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
+		if instance.context.version == 18:
+			yield 'pathtype', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'pathmaterial', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'pathextrusion_kerb', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'pathextrusion_railing', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
