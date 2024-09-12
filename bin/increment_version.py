b38fbfb45db985add60d3aab43830f8172a88f5f
Zach Guo <3478203+zachguo@users.noreply.github.com>
2024-09-12 06:37:39 +0000

    dev - only increment version for user-facing changes

--- bin/increment_version.py
+++ bin/increment_version.py
@@ -3,17 +3,46 @@ import subprocess
 from __version__ import VERSION
 
 
+def should_increment():
+    TRACKED_FOLDERS = [
+        "codegen",
+        "constants",
+        "generated",
+        "source",
+        "modules",
+        "ovl_util",
+        "dumps",
+        "sql_commands",
+        "gui",
+        "plugin",
+    ]
+    changed = subprocess.check_output(
+        ["git", "diff", "--name-only", "--cached"], text=True
+    )
+    for path in changed.splitlines():
+        path = path.split("/")
+        if path[0] in TRACKED_FOLDERS:
+            return True
+        elif len(path) == 1 and path[0].endswith(".py"):
+            return True
+
+
 def main():
-    last_commit = subprocess.check_output(["git", "log", r"--pretty=format:%h - %cd", "-1"], text=True)
-    commit_hash, commit_time = last_commit.split(" - ")
-    version_file = "__version__.py"
-    with open(version_file, "w") as file:
-        file.writelines([
-            f'VERSION = "{VERSION}"\n',
-            f'COMMIT_HASH = "{commit_hash}"\n',
-            f'COMMIT_TIME = "{commit_time}"\n',
-        ])
-    return subprocess.run(["git", "add", version_file]).returncode
+    if should_increment():
+        last_commit = subprocess.check_output(
+            ["git", "log", r"--pretty=format:%h - %cd", "-1"], text=True
+        )
+        commit_hash, commit_time = last_commit.split(" - ")
+        version_file = "__version__.py"
+        with open(version_file, "w") as file:
+            file.writelines(
+                [
+                    f'VERSION = "{VERSION}"\n',
+                    f'COMMIT_HASH = "{commit_hash}"\n',
+                    f'COMMIT_TIME = "{commit_time}"\n',
+                ]
+            )
+        return subprocess.run(["git", "add", version_file]).returncode
 
 
 if __name__ == "__main__":
