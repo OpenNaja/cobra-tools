4b32f6ad2d3a9da5998a806b6cab7dc4ff6ab037
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-09-25 15:07:56 +0200

    gui - #392 add experimental steam launching code

--- gui/widgets.py
+++ gui/widgets.py
@@ -2139,10 +2139,36 @@ class GamesWidget(QWidget):
     def run_selected_game(self):
         selected_game = self.get_selected_game()
         logging.info(f"Running {selected_game}")
+        args = ["-nointro", ]
+        # todo - maybe support custom args per game?
+        # args = ["-nointro", "-disableprint", "-level", "modelviewer"]  # only works on JWE2, crashes PC, PZ
+        # todo - investigate steam launcher more?
+        # # try steam launch
+        # id_map = {
+        #     "Jurassic World Evolution 2": 1244460,
+        #     "Jurassic World Evolution": 648350,
+        #     "Planet Coaster 2": 2688950,
+        #     "Planet Coaster": 493340,
+        #     "Planet Zoo": 703080,
+        #     "Disneyland Adventures": 630610,
+        #     "Zoo Tycoon Ultimate Animal Collection": 613880,
+        #     }
+        # game_id = id_map.get(selected_game)
+        # if game_id:
+        #     import webbrowser
+        #     args_str = "//".join(args)
+        #     steam_cmd = f"steam://rungameid/{game_id}//{args_str}"
+        #     logging.debug(f"Running game from {steam_cmd}")
+        #     webbrowser.open(steam_cmd)
+        #     # subprocess.run(f"start steam://rungameid/{game_id}", *args, shell=True)
+        # else:
         for game, ovldata in self.cfg["games"].items():
             if game == selected_game:
                 exe_path = self.get_exe_from_ovldata(ovldata)
-                subprocess.Popen([exe_path, "-nointro"])
+                subprocess.Popen([exe_path, ] + args)
+                break
+        else:
+            logging.warning(f"Couldn't find the game's executable")
 
     def set_root(self, dir_game: str) -> None:
         root_index = self.model.setRootPath(dir_game)
