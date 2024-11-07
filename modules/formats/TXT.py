66f60c32a4da5548f20393def28b2252892f1da8
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-11-07 15:29:54 +0100

    txt - fix PC2

--- modules/formats/TXT.py
+++ modules/formats/TXT.py
@@ -16,11 +16,17 @@ class TxtLoader(BaseFile):
 		pool, offset = self.root_ptr
 		b = pool.get_data_at(offset)
 		# sized strings
-		if is_dla(self.ovl):
+		if self.mime_version == 1:
+			# DLA
 			# unk = 128, usually
 			size, unk = struct.unpack("<2B", b[:2])
 			data = b[2:2+size*2]
+		elif self.mime_version >= 4:
+			# PC2
+			size = struct.unpack("<Q", b[:8])[0]
+			data = b[8:8+size]
 		else:
+			# version 2 (PC1, JWE1) & 3 (PZ, JWE2, WH)
 			size = struct.unpack("<I", b[:4])[0]
 			data = b[4:4+size]
 		out_path = out_dir(self.name)
@@ -31,5 +37,6 @@ class TxtLoader(BaseFile):
 	def _get_data(self, file_path):
 		"""Loads and returns the data for a TXT"""
 		raw_txt_bytes = self.get_content(file_path)
-		root_entry = struct.pack("<I", len(raw_txt_bytes)) + raw_txt_bytes + b"\x00"
+		fmt = "<I" if self.mime_version in (2, 3) else "<Q"
+		root_entry = struct.pack(fmt, len(raw_txt_bytes)) + raw_txt_bytes + b"\x00"
 		return root_entry + get_padding(len(root_entry), alignment=8)
