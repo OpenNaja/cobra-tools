f71f796cd79ff1cde6a0149130a3d203d93988cf
ilodev <ilodev@users.noreply.github.com>
2024-11-17 18:28:29 +0100

    Updated trackelement to the correct size

--- generated/formats/trackelement/compounds/TrackElementRoot.py
+++ generated/formats/trackelement/compounds/TrackElementRoot.py
@@ -13,8 +13,8 @@ class TrackElementRoot(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.count = name_type_map['Uint64'](self.context, 0, None)
-		self.track_data = name_type_map['ArrayPointer'](self.context, self.count, name_type_map['TrackElementData'])
+		self.track_data_count = name_type_map['Uint64'](self.context, 0, None)
+		self.track_data = name_type_map['ArrayPointer'](self.context, self.track_data_count, name_type_map['TrackElementData'])
 		self.support_prefab_name_1 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.support_prefab_name_2 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
@@ -24,14 +24,14 @@ class TrackElementRoot(MemStruct):
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
 		yield 'track_data', name_type_map['ArrayPointer'], (None, name_type_map['TrackElementData']), (False, None), (None, None)
-		yield 'count', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'track_data_count', name_type_map['Uint64'], (0, None), (False, None), (None, None)
 		yield 'support_prefab_name_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'support_prefab_name_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
-		yield 'track_data', name_type_map['ArrayPointer'], (instance.count, name_type_map['TrackElementData']), (False, None)
-		yield 'count', name_type_map['Uint64'], (0, None), (False, None)
+		yield 'track_data', name_type_map['ArrayPointer'], (instance.track_data_count, name_type_map['TrackElementData']), (False, None)
+		yield 'track_data_count', name_type_map['Uint64'], (0, None), (False, None)
 		yield 'support_prefab_name_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'support_prefab_name_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
