cde39f379b970d6680cee984888cb9e6b2e50e0a
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-12-08 14:22:17 +0100

    improve txt for ZTUAC

--- modules/formats/TXT.py
+++ modules/formats/TXT.py
@@ -21,14 +21,16 @@ class TxtLoader(BaseFile):
 			# unk = 128, usually
 			size, unk = struct.unpack("<2B", b[:2])
 			data = b[2:2+size*2]
+		elif self.mime_version in (2, 3):
+			# version 2 (PC1, JWE1) & 3 (PZ, JWE2, WH)
+			size = struct.unpack("<I", b[:4])[0]
+			data = b[4:4+size]
 		elif self.mime_version >= 4:
 			# PC2
 			size = struct.unpack("<Q", b[:8])[0]
 			data = b[8:8+size]
 		else:
-			# version 2 (PC1, JWE1) & 3 (PZ, JWE2, WH)
-			size = struct.unpack("<I", b[:4])[0]
-			data = b[4:4+size]
+			raise AttributeError(f"Unknown TXT format {self.mime_version}")
 		out_path = out_dir(self.name)
 		with open(out_path, "wb") as f:
 			f.write(data)
@@ -37,6 +39,13 @@ class TxtLoader(BaseFile):
 	def _get_data(self, file_path):
 		"""Loads and returns the data for a TXT"""
 		raw_txt_bytes = self.get_content(file_path)
-		fmt = "<I" if self.mime_version in (2, 3) else "<Q"
-		root_entry = struct.pack(fmt, len(raw_txt_bytes)) + raw_txt_bytes + b"\x00"
+		if self.mime_version == 1:
+			prefix = struct.pack("<2B", len(raw_txt_bytes), 128)
+		elif self.mime_version in (2, 3):
+			prefix = struct.pack("<I", len(raw_txt_bytes))
+		elif self.mime_version == 4:
+			prefix = struct.pack("<Q", len(raw_txt_bytes))
+		else:
+			raise AttributeError(f"Unknown TXT format {self.mime_version}")
+		root_entry = prefix + raw_txt_bytes + b"\x00"
 		return root_entry + get_padding(len(root_entry), alignment=8)
