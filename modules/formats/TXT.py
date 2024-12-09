bfbf2d09b8955e74c6a7fb954ffd3e377a2b37b0
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2024-12-09 20:25:52 +0100

    improve txt for pc2

--- modules/formats/TXT.py
+++ modules/formats/TXT.py
@@ -4,7 +4,8 @@ from modules.formats.BaseFormat import BaseFile
 
 UTF8 = "utf-8"
 UTF16 = "utf-16-le"  # 00 bytes is second
-
+SENTIMENT = " [sentiment="
+NULL = b"\x00"
 
 class TxtLoader(BaseFile):
 	extension = ".txt"
@@ -25,6 +26,7 @@ class TxtLoader(BaseFile):
 		pool, offset = self.root_ptr
 		b = pool.get_data_at(offset)
 		# sized strings
+		sentiment = None
 		if self.mime_version == 1:
 			# DLA uses utf16, so every second byte is 00, eg b'C\x00a\x00m\x00e\x00r\x00a\x00'
 			char_count, count_mask = struct.unpack("<2B", b[:2])
@@ -39,20 +41,29 @@ class TxtLoader(BaseFile):
 			data = b[4:4+char_count]
 		elif self.mime_version == 4:
 			# PC2
-			char_count = struct.unpack("<Q", b[:8])[0]
+			char_count, sentiment_char_count = struct.unpack("<II", b[:8])
 			data = b[8:8+char_count]
+			sentiment = b[9+char_count:9+char_count+sentiment_char_count]
 		else:
 			raise AttributeError(f"Unknown TXT format {self.mime_version}")
 		out_path = out_dir(self.name)
 		with open(out_path, "w", encoding=UTF8) as f:
 			f.write(data.decode(self.encoding))
+			if sentiment:
+				f.write(f"{SENTIMENT}{sentiment.decode(self.encoding)}]")
 		return out_path,
 
 	def _get_data(self, file_path):
 		"""Loads and returns the data for a TXT"""
 		with open(file_path, 'r', encoding=UTF8) as f:
 			txt_str = f.read()
+		sentiment_str = ""
+		if self.mime_version == 4:
+			if SENTIMENT in txt_str:
+				txt_str, sentiment_str = txt_str.split(SENTIMENT)
+				sentiment_str = sentiment_str[:-1]  # remove ]
 		txt_bytes = txt_str.encode(self.encoding)
+		sentiment_bytes = sentiment_str.encode(self.encoding)
 		if self.mime_version == 1:
 			# take char count, not byte count
 			len_raw = len(txt_str)
@@ -63,8 +74,10 @@ class TxtLoader(BaseFile):
 		elif self.mime_version in (2, 3):
 			prefix = struct.pack("<I", len(txt_bytes))
 		elif self.mime_version == 4:
-			prefix = struct.pack("<Q", len(txt_bytes))
+			prefix = struct.pack("<II", len(txt_bytes), len(sentiment_bytes))
 		else:
 			raise AttributeError(f"Unknown TXT format {self.mime_version}")
-		root_entry = prefix + txt_bytes + b"\x00"
+		root_entry = prefix + txt_bytes + NULL
+		if sentiment_bytes:
+			root_entry += sentiment_bytes + NULL
 		return root_entry + get_padding(len(root_entry), alignment=8)
