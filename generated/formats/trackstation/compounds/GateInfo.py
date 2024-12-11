840d1d46e8b36212f76247b5de6b38973796b14e
ilodev <ilodev@users.noreply.github.com>
2024-12-11 23:01:32 +0100

    Fix flume station info, and simplified .trackstation in general

--- generated/formats/trackstation/compounds/GateInfo.py
+++ generated/formats/trackstation/compounds/GateInfo.py
@@ -10,34 +10,34 @@ class GateInfo(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.unk_ints_2 = Array(self.context, 0, None, (0,), name_type_map['Uint'])
 		self.floats = Array(self.context, 0, None, (0,), name_type_map['Float'])
-		self.entrance = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.exit = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.unk_1 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.unk_2 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.unk_3 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.entrance_gate = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.exit_gate = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.unknown_ptr = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.fence_extrusion = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.small_fence_extrusion = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.fence_cap = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
 			self.set_defaults()
 
 	@classmethod
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
-		yield 'entrance', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'exit', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'unk_ints_2', Array, (0, None, (2,), name_type_map['Uint']), (False, None), (None, None)
-		yield 'unk_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'unk_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
-		yield 'unk_3', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'entrance_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'exit_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'unknown_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (True, None), (None, None)
+		yield 'fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'small_fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'fence_cap', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'floats', Array, (0, None, (4,), name_type_map['Float']), (False, None), (None, None)
 
 	@classmethod
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
-		yield 'entrance', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'exit', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'unk_ints_2', Array, (0, None, (2,), name_type_map['Uint']), (False, None)
-		yield 'unk_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'unk_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-		yield 'unk_3', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'entrance_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'exit_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'unknown_ptr', name_type_map['Pointer'], (0, name_type_map['ZString']), (True, None)
+		yield 'fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'small_fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		yield 'fence_cap', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'floats', Array, (0, None, (4,), name_type_map['Float']), (False, None)
--- generated/formats/trackstation/compounds/TrackStationRoot.py
+++ generated/formats/trackstation/compounds/TrackStationRoot.py
@@ -11,11 +11,10 @@ class TrackStationRoot(MemStruct):
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
 		self.station_grid_sizes = Array(self.context, 0, None, (0,), name_type_map['Float'])
-		self.unknown_ints = Array(self.context, 0, None, (0,), name_type_map['Uint'])
-		self.unknown_ptr = name_type_map['Uint64'](self.context, 0, None)
-		self.unknown_56 = name_type_map['Uint64'](self.context, 0, None)
-		self.unknown_64 = name_type_map['Uint64'](self.context, 0, None)
-		self.unknown_72 = name_type_map['Uint64'](self.context, 0, None)
+		self.flags = name_type_map['Uint64'](self.context, 0, None)
+		self.unknown_ptr = name_type_map['Uint64'].from_value(0)
+		self.unknown_56 = name_type_map['Uint64'].from_value(0)
+		self.unknown_72 = name_type_map['Uint64'].from_value(0)
 		self.unk_ints_2 = Array(self.context, 0, None, (0,), name_type_map['Uint'])
 		self.unk_floats_2 = Array(self.context, 0, None, (0,), name_type_map['Float'])
 		self.unk_floats_3 = Array(self.context, 0, None, (0,), name_type_map['Uint'])
@@ -27,9 +26,10 @@ class TrackStationRoot(MemStruct):
 		self.gate_info = name_type_map['Pointer'](self.context, 0, name_type_map['GateInfo'])
 		self.control_box_right_panel = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.entrance_gate = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.flume_info = name_type_map['Pointer'](self.context, 0, name_type_map['FlumeInfo'])
 		self.exit_gate = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.fence_ext = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
-		self.small_fence_ext = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.fence_extrusion = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.small_fence_extrusion = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.fence_cap = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		if set_default:
 			self.set_defaults()
@@ -38,8 +38,8 @@ class TrackStationRoot(MemStruct):
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
 		yield 'station_grid_sizes', Array, (0, None, (2,), name_type_map['Float']), (False, None), (None, None)
-		yield 'unknown_ints', Array, (0, None, (2,), name_type_map['Uint']), (False, None), (None, None)
-		yield 'unknown_ptr', name_type_map['Uint64'], (0, None), (False, None), (lambda context: context.is_pc_2, None)
+		yield 'flags', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'unknown_ptr', name_type_map['Uint64'], (0, None), (True, 0), (lambda context: context.is_pc_2, None)
 		yield 'corner_edge_track', name_type_map['Pointer'], (0, name_type_map['CornerEdgeTrack']), (False, None), (lambda context: not context.is_pc_2, None)
 		yield 'corner_edge_track', name_type_map['Pointer'], (0, name_type_map['CornerEdgeTrack']), (False, None), (lambda context: context.is_pc_2, None)
 		yield 'track_only', name_type_map['Pointer'], (0, name_type_map['TrackOnly']), (False, None), (lambda context: not context.is_pc_2, None)
@@ -49,14 +49,14 @@ class TrackStationRoot(MemStruct):
 		yield 'control_box_left_panel', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
 		yield 'gate_info', name_type_map['Pointer'], (0, name_type_map['GateInfo']), (False, None), (lambda context: context.is_pc_2, None)
 		yield 'control_box_right_panel', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
-		yield 'unknown_56', name_type_map['Uint64'], (0, None), (False, None), (lambda context: context.is_pc_2, None)
+		yield 'unknown_56', name_type_map['Uint64'], (0, None), (True, 0), (lambda context: context.is_pc_2, None)
 		yield 'entrance_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
-		yield 'unknown_64', name_type_map['Uint64'], (0, None), (False, None), (lambda context: context.is_pc_2, None)
+		yield 'flume_info', name_type_map['Pointer'], (0, name_type_map['FlumeInfo']), (False, None), (lambda context: context.is_pc_2, None)
 		yield 'exit_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
-		yield 'unknown_72', name_type_map['Uint64'], (0, None), (False, None), (lambda context: context.is_pc_2, None)
+		yield 'unknown_72', name_type_map['Uint64'], (0, None), (True, 0), (lambda context: context.is_pc_2, None)
 		yield 'unk_ints_2', Array, (0, None, (2,), name_type_map['Uint']), (False, None), (lambda context: not context.is_pc_2, None)
-		yield 'fence_ext', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
-		yield 'small_fence_ext', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
+		yield 'fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
+		yield 'small_fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
 		yield 'fence_cap', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: not context.is_pc_2, None)
 		yield 'unk_floats_2', Array, (0, None, (4,), name_type_map['Float']), (False, None), (lambda context: not context.is_pc_2, None)
 		yield 'unk_floats_3', Array, (0, None, (2,), name_type_map['Uint']), (False, None), (lambda context: not context.is_pc_2, None)
@@ -65,9 +65,9 @@ class TrackStationRoot(MemStruct):
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'station_grid_sizes', Array, (0, None, (2,), name_type_map['Float']), (False, None)
-		yield 'unknown_ints', Array, (0, None, (2,), name_type_map['Uint']), (False, None)
+		yield 'flags', name_type_map['Uint64'], (0, None), (False, None)
 		if instance.context.is_pc_2:
-			yield 'unknown_ptr', name_type_map['Uint64'], (0, None), (False, None)
+			yield 'unknown_ptr', name_type_map['Uint64'], (0, None), (True, 0)
 		if not instance.context.is_pc_2:
 			yield 'corner_edge_track', name_type_map['Pointer'], (0, name_type_map['CornerEdgeTrack']), (False, None)
 		if instance.context.is_pc_2:
@@ -87,19 +87,19 @@ class TrackStationRoot(MemStruct):
 		if not instance.context.is_pc_2:
 			yield 'control_box_right_panel', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		if instance.context.is_pc_2:
-			yield 'unknown_56', name_type_map['Uint64'], (0, None), (False, None)
+			yield 'unknown_56', name_type_map['Uint64'], (0, None), (True, 0)
 		if not instance.context.is_pc_2:
 			yield 'entrance_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		if instance.context.is_pc_2:
-			yield 'unknown_64', name_type_map['Uint64'], (0, None), (False, None)
+			yield 'flume_info', name_type_map['Pointer'], (0, name_type_map['FlumeInfo']), (False, None)
 		if not instance.context.is_pc_2:
 			yield 'exit_gate', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		if instance.context.is_pc_2:
-			yield 'unknown_72', name_type_map['Uint64'], (0, None), (False, None)
+			yield 'unknown_72', name_type_map['Uint64'], (0, None), (True, 0)
 		if not instance.context.is_pc_2:
 			yield 'unk_ints_2', Array, (0, None, (2,), name_type_map['Uint']), (False, None)
-			yield 'fence_ext', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
-			yield 'small_fence_ext', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+			yield 'fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+			yield 'small_fence_extrusion', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 			yield 'fence_cap', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 			yield 'unk_floats_2', Array, (0, None, (4,), name_type_map['Float']), (False, None)
 			yield 'unk_floats_3', Array, (0, None, (2,), name_type_map['Uint']), (False, None)
