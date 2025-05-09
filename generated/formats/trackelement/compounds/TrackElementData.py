a0068131e37d26d44e3a786ef591b68334d35f31
HENDRIX-ZT2 <HENDRIX-ZT2@users.noreply.github.com>
2025-05-09 18:23:22 +0200

    Improve trackelement for PC2

--- generated/formats/trackelement/compounds/TrackElementData.py
+++ generated/formats/trackelement/compounds/TrackElementData.py
@@ -5,7 +5,9 @@ from generated.formats.trackelement.imports import name_type_map
 class TrackElementData(MemStruct):
 
 	"""
-	PC: was 80, now it is 72, PZ: 48
+	PC: was 80, now it is 72
+	PZ: 48
+	PC2: 88
 	"""
 
 	__name__ = 'TrackElementData'
@@ -13,7 +15,8 @@ class TrackElementData(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.trackmeshlist_count = name_type_map['Uint64'](self.context, 0, None)
+		self.trackmeshlist_count = name_type_map['Uint'](self.context, 0, None)
+		self.unk_count = name_type_map['Uint'](self.context, 0, None)
 		self.direction = name_type_map['Uint'](self.context, 0, None)
 		self.unk_2 = name_type_map['Uint'](self.context, 0, None)
 		self.unk_3 = name_type_map['Ushort'].from_value(0)
@@ -24,6 +27,8 @@ class TrackElementData(MemStruct):
 		self.offset = name_type_map['Float'](self.context, 0, None)
 		self.unk_9 = name_type_map['Uint'].from_value(0)
 		self.spline_name = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.u_1 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
+		self.u_2 = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.trackmesh_element_name = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
 		self.trackmeshlist = name_type_map['ArrayPointer'](self.context, self.trackmeshlist_count, name_type_map['TrackMeshRef'])
 		self.optional_catwalk = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
@@ -34,9 +39,12 @@ class TrackElementData(MemStruct):
 	def _get_attribute_list(cls):
 		yield from super()._get_attribute_list()
 		yield 'spline_name', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
+		yield 'u_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: context.version >= 12, None)
+		yield 'u_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (lambda context: context.version >= 12, None)
 		yield 'trackmesh_element_name', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'trackmeshlist', name_type_map['ArrayPointer'], (None, name_type_map['TrackMeshRef']), (False, None), (None, None)
-		yield 'trackmeshlist_count', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'trackmeshlist_count', name_type_map['Uint'], (0, None), (False, None), (None, None)
+		yield 'unk_count', name_type_map['Uint'], (0, None), (False, None), (None, None)
 		yield 'optional_catwalk', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'direction', name_type_map['Uint'], (0, None), (False, None), (None, None)
 		yield 'unk_2', name_type_map['Uint'], (0, None), (False, None), (None, None)
@@ -52,9 +60,13 @@ class TrackElementData(MemStruct):
 	def _get_filtered_attribute_list(cls, instance, include_abstract=True):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'spline_name', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+		if instance.context.version >= 12:
+			yield 'u_1', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
+			yield 'u_2', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'trackmesh_element_name', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'trackmeshlist', name_type_map['ArrayPointer'], (instance.trackmeshlist_count, name_type_map['TrackMeshRef']), (False, None)
-		yield 'trackmeshlist_count', name_type_map['Uint64'], (0, None), (False, None)
+		yield 'trackmeshlist_count', name_type_map['Uint'], (0, None), (False, None)
+		yield 'unk_count', name_type_map['Uint'], (0, None), (False, None)
 		yield 'optional_catwalk', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'direction', name_type_map['Uint'], (0, None), (False, None)
 		yield 'unk_2', name_type_map['Uint'], (0, None), (False, None)
