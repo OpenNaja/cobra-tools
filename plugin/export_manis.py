54f6c48d71246434221c153ad3d41f13b4be0a32
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-05-31 14:20:00 +0200

    manis - improve var names

--- plugin/export_manis.py
+++ plugin/export_manis.py
@@ -27,11 +27,11 @@ defaults_keys = {
 }
 
 
-def store_pose_frame_info(b_ob, src_i, trg_i, bones_data, channel_storage, corrector, cam_corr):
-	bpy.context.scene.frame_set(src_i)
+def store_pose_frame_info(b_ob, src_frame, trg_frame, bones_data, channel_storage, corrector, cam_corr):
+	bpy.context.scene.frame_set(src_frame)
 	bpy.context.view_layer.update()
 	if b_ob.type == "CAMERA":
-		store_transform_data(channel_storage, corrector, b_ob.matrix_local, "camera_joint", src_i, trg_i, cam_corr)
+		store_transform_data(channel_storage, corrector, b_ob.matrix_local, "camera_joint", src_frame, trg_frame, cam_corr)
 	elif b_ob.type == "ARMATURE":
 		for b_name, p_bone in b_ob.pose.bones.items():
 			m_name = bone_name_for_ovl(b_name)
@@ -41,13 +41,13 @@ def store_pose_frame_info(b_ob, src_i, trg_i, bones_data, channel_storage, corre
 			# whereas blender stores translation relative to the bone itself, not the parent
 			matrix = bones_data[m_name] @ b_ob.convert_space(
 				pose_bone=p_bone, matrix=p_bone.matrix, from_space='POSE', to_space='LOCAL')
-			store_transform_data(channel_storage, corrector, matrix, m_name, src_i, trg_i, cam_corr)
+			store_transform_data(channel_storage, corrector, matrix, m_name, src_frame, trg_frame, cam_corr)
 
 
-def store_transform_data(channel_storage, corrector, matrix, name, src_i, trg_i, cam_corr):
+def store_transform_data(channel_storage, corrector, matrix, name, src_frame, trg_frame, cam_corr):
 	# loc
-	v = sample_scale2(matrix, src_i, inverted=True) @ matrix
-	channel_storage[name][POS][trg_i] = corrector.from_blender(v).to_translation()
+	v = sample_scale2(matrix, src_frame, inverted=True) @ matrix
+	channel_storage[name][POS][trg_frame] = corrector.from_blender(v).to_translation()
 	# rot
 	final_m = corrector.from_blender(matrix)
 	key = final_m.to_quaternion()
@@ -56,16 +56,16 @@ def store_transform_data(channel_storage, corrector, matrix, name, src_i, trg_i,
 		key.rotate(out)
 		key = mathutils.Quaternion((key.x, -key.y, key.w, -key.z))
 	# swizzle - w is stored last
-	channel_storage[name][ORI][trg_i] = key.x, key.y, key.z, key.w
+	channel_storage[name][ORI][trg_frame] = key.x, key.y, key.z, key.w
 	if name == srb_name:
-		channel_storage[name][EUL][trg_i] = key.to_euler()
+		channel_storage[name][EUL][trg_frame] = key.to_euler()
 	# scale
-	scale_mat = sample_scale2(matrix, src_i)
+	scale_mat = sample_scale2(matrix, src_frame)
 	# needs axis correction, but appears to be stored relative to the animated bone's axes
 	scale_mat = corrector.from_blender(scale_mat)
 	# swizzle
 	key = scale_mat.to_scale()
-	channel_storage[name][SCL][trg_i] = key.z, key.y, key.x
+	channel_storage[name][SCL][trg_frame] = key.z, key.y, key.x
 
 
 def reasonably_close(a, b):
@@ -226,8 +226,8 @@ def export_actions(b_ob, actions, manis, mani_infos, folder, scene):
 			channel_storage[srb_name][EUL] = np.zeros((mani_info.frame_count, 3), float)
 		# store pose data for b_action
 		b_ob.animation_data.action = b_action
-		for trg_i, src_i in enumerate(range(first_frame, last_frame)):
-			store_pose_frame_info(b_ob, src_i, trg_i, bones_data, channel_storage, corrector, cam_corr)
+		for trg_frame, src_frame in enumerate(range(first_frame, last_frame)):
+			store_pose_frame_info(b_ob, src_frame, trg_frame, bones_data, channel_storage, corrector, cam_corr)
 
 		# decide which channels to keyframe by determining if the keys are static
 		for bone, channels in tuple(channel_storage.items()):
