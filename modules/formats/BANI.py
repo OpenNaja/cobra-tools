83746ffcf095c570077377190a7ceb4bd893a9e1
HENDRIX-ZT2 <HENDRIX-ZT2@users.noreply.github.com>
2025-06-26 20:31:34 +0200

    banis - vectorize banis

--- modules/formats/BANI.py
+++ modules/formats/BANI.py
@@ -38,11 +38,15 @@ class BanisLoader(MimeVersionedLoader):
 
 	def collect(self):
 		if self.mime_version == 7 and self.name != "animation.baniset7b77b17d.banis":
-			# un-vectorized arrays are extremely slow eg for guests
+			# big banis are extremely slow even when vectorized, e.g. for guests
 			logging.warning(f"Skipping loading of {self.name} temporarily to speed up ovl loading")
+			pool, offset = self.root_ptr
+			stream = pool.stream_at(offset)
+			self.header = self.target_class.from_stream(stream, self.context)
+			print(self.header)
+			# keys_size does not completely line up with the counts, maybe re-using keys?
 			return
 		super().collect()
-		# print(self.header)
 
 	def validate(self):
 		self.extra_loaders = []
