3fbbe531449ce00ff1b0e96b734965e5daba46a1
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-09-07 12:22:51 +0200

    improve motiongraph for pc2

--- generated/formats/motiongraph/compounds/SelectActivityActivityData.py
+++ generated/formats/motiongraph/compounds/SelectActivityActivityData.py
@@ -13,7 +13,7 @@ class SelectActivityActivityData(MemStruct):
 
 	def __init__(self, context, arg=0, template=None, set_default=True):
 		super().__init__(context, arg, template, set_default=False)
-		self.num_activities = name_type_map['Uint64'](self.context, 0, None)
+		self.num_activities = name_type_map['Uint'](self.context, 0, None)
 		self.blend_time = name_type_map['Float'](self.context, 0, None)
 		self.mode = name_type_map['SelectActivityActivityMode'](self.context, 0, None)
 		self.enum_variable = name_type_map['Pointer'](self.context, 0, name_type_map['ZString'])
@@ -26,7 +26,7 @@ class SelectActivityActivityData(MemStruct):
 		yield from super()._get_attribute_list()
 		yield 'enum_variable', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None), (None, None)
 		yield 'activities', name_type_map['Pointer'], (None, name_type_map['ActivitiesLinks']), (False, None), (None, None)
-		yield 'num_activities', name_type_map['Uint64'], (0, None), (False, None), (None, None)
+		yield 'num_activities', name_type_map['Uint'], (0, None), (False, None), (None, None)
 		yield 'blend_time', name_type_map['Float'], (0, None), (False, None), (None, None)
 		yield 'mode', name_type_map['SelectActivityActivityMode'], (0, None), (False, None), (None, None)
 
@@ -35,6 +35,6 @@ class SelectActivityActivityData(MemStruct):
 		yield from super()._get_filtered_attribute_list(instance, include_abstract)
 		yield 'enum_variable', name_type_map['Pointer'], (0, name_type_map['ZString']), (False, None)
 		yield 'activities', name_type_map['Pointer'], (instance.num_activities, name_type_map['ActivitiesLinks']), (False, None)
-		yield 'num_activities', name_type_map['Uint64'], (0, None), (False, None)
+		yield 'num_activities', name_type_map['Uint'], (0, None), (False, None)
 		yield 'blend_time', name_type_map['Float'], (0, None), (False, None)
 		yield 'mode', name_type_map['SelectActivityActivityMode'], (0, None), (False, None)
