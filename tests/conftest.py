0fd581476152828e13f1faf01349363f9a177aee
Jon <170077+hexabits@users.noreply.github.com>
2026-08-05 15:17:25 -0400

    [DEV] Add fake-bpy-module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,14 @@
+import sys
+try:
+	import bpy
+	# fake-bpy-module lacks the 'app' attribute
+	if not hasattr(bpy, "app"):
+		# Remove the loaded fake module
+		del sys.modules["bpy"]
+		# Force subsequent imports of 'bpy' to raise ModuleNotFoundError
+		sys.modules["bpy"] = None
+except ImportError:
+	pass
 
 pytest_plugins = [
 	"tests.fixtures.missing_modules",
