9008c86bf2a261cd84dafdb35f29f83caf4b833b
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2026-08-14 10:52:49 +0200

    [Plugin] Clean and document get_bone_bind_data

--- plugin/utils/anim.py
+++ plugin/utils/anim.py
@@ -1,11 +1,14 @@
 import math
 
+import bpy
 import mathutils
 
 
-def get_bone_bind_data(b_armature_ob, bones_table, corrector):
-	binds = []
-	bones_local_mat = []
+def get_bone_bind_data(b_armature_ob : bpy.types.Object, bones_table, corrector) -> tuple[list[mathutils.Matrix], list[mathutils.Matrix]]:
+	"""Returns a list of bind matrices in game's armature space according to corrector and
+	a list of local matrices in blender's local space (relative to the parent bone)."""
+	g_bind_matrices = []
+	b_local_matrices = []
 	for bone_i, bone_name in bones_table:
 		if bone_name in b_armature_ob.data.bones:
 			b_bone = b_armature_ob.data.bones[bone_name]
@@ -13,16 +16,12 @@ def get_bone_bind_data(b_armature_ob, bones_table, corrector):
 			b_local = mathutils.Matrix(b_bind)
 			if b_bone.parent:
 				b_local = b_bone.parent.matrix_local.inverted() @ b_local
-			bones_local_mat.append(b_local)
-			bind = corrector.from_blender(b_bind)
-			# inv_bind = bind.inverted()
-			# bind_loc = b_armature_ob.data.bones[bone_name].matrix_local.translation
-			# bind_loc_inv = bind_loc.negate()
-			binds.append(bind)
+			b_local_matrices.append(b_local)
+			g_bind_matrices.append(corrector.from_blender(b_bind))
 		else:
-			bones_local_mat.append(mathutils.Matrix().to_4x4())
-			binds.append(mathutils.Matrix().to_4x4())
-	return binds, bones_local_mat
+			b_local_matrices.append(mathutils.Matrix().to_4x4())
+			g_bind_matrices.append(mathutils.Matrix().to_4x4())
+	return g_bind_matrices, b_local_matrices
 
 
 c_map = (
